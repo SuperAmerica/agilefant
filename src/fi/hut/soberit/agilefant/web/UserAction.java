31aa6b7a0e6bfa274bd32b96adc96f2fb51cb0de
kheleniu <kheleniu@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2006-11-14 16:27:41 +0000

    Added stub for UserAction.
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@72 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- src/fi/hut/soberit/agilefant/web/UserAction.java
+++ src/fi/hut/soberit/agilefant/web/UserAction.java
@@ -2,6 +2,52 @@ package fi.hut.soberit.agilefant.web;
 
 import com.opensymphony.xwork.ActionSupport;
 
-public class UserAction extends ActionSupport {
+import fi.hut.soberit.agilefant.db.UserDAO;
+import fi.hut.soberit.agilefant.model.User;
 
+public class UserAction extends ActionSupport implements CRUDAction{
+	
+	private int userId;
+	private User user;
+	private UserDAO userDAO;
+
+	public String create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String delete() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String edit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String store() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public void setUserDAO(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
 }

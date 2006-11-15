6dc15b4712d54fd2e257fc14b2845c9633f7ca64
kheleniu <kheleniu@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2006-11-15 15:21:28 +0000

    Added abstract SpringTagSupport which can be used to access Spring ApplicationContext.
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@73 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- src/fi/hut/soberit/agilefant/web/tag/ProductListTag.java
+++ src/fi/hut/soberit/agilefant/web/tag/ProductListTag.java
@@ -1,34 +1,17 @@
 package fi.hut.soberit.agilefant.web.tag;
 
 import javax.servlet.jsp.JspException;
-import javax.servlet.jsp.PageContext;
 import javax.servlet.jsp.tagext.Tag;
-import javax.servlet.jsp.tagext.TagSupport;
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-
 import fi.hut.soberit.agilefant.db.ProductDAO;
 
-public class ProductListTag extends TagSupport{
+public class ProductListTag extends SpringTagSupport{
 	
 	private ProductDAO productDAO;
-	private PageContext pageContext;
 		
 	@Override
 	public int doStartTag() throws JspException {
-		pageContext.setAttribute("productList", productDAO.getAll());
+		productDAO = (ProductDAO)super.getApplicationContext().getBean("productDAO");
+		super.getPageContext().setAttribute("productList", productDAO.getAll());
 		return Tag.EVAL_BODY_INCLUDE;
-	}
-
-	@Override
-	public void setPageContext(PageContext ctx) {
-		this.pageContext = ctx;
-		productDAO = (ProductDAO)this.getApplicationContext(ctx).getBean("productDAO");
-		super.setPageContext(ctx);
-	}
-	
-	protected ApplicationContext getApplicationContext(PageContext pageContext){
-		return WebApplicationContextUtils.getWebApplicationContext(pageContext.getServletContext());
 	}
 }
---
+++ src/fi/hut/soberit/agilefant/web/tag/SpringTagSupport.java
@@ -0,0 +1,27 @@
+package fi.hut.soberit.agilefant.web.tag;
+
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+public abstract class SpringTagSupport extends TagSupport {
+	
+	private PageContext pageContext;
+	private ApplicationContext applicationContext;
+	
+	@Override
+	public final void setPageContext(PageContext pageContext) {
+		super.setPageContext(pageContext);
+		this.applicationContext = WebApplicationContextUtils.getWebApplicationContext(pageContext.getServletContext());
+	}
+	
+	protected PageContext getPageContext(){
+		return this.pageContext;
+	}
+	
+	protected ApplicationContext getApplicationContext(){
+		return applicationContext;
+	}
+}

f35bff33ade0058398f5f565e0349f1753318fb5
jmrantal <jmrantal@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2006-11-26 21:24:15 +0000

    layout stuff
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@90 29784df9-99d5-7e28-3e6c-ac1830307ffc

---
+++ web/static/js/generic.js
@@ -0,0 +1,15 @@
+function MM_findObj(n, d) { // v4.01 //
+ var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+ d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+ if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+ for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+ if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+function MM_reloadPage(init) {  // reloads the window if Nav4 resized //
+  if (init==true) with (navigator) {if ((appName=="Netscape")&&(parseInt(appVersion)==4)) {
+    document.MM_pgW=innerWidth; document.MM_pgH=innerHeight; onresize=MM_reloadPage; }}
+  else if (innerWidth!=document.MM_pgW || innerHeight!=document.MM_pgH) location.reload();
+}
+MM_reloadPage(true);
+
+

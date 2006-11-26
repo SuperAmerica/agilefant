4af5baf5dd2ab7c418f9d5b2aaea40039e1b8cc0
ekantola <ekantola@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2006-11-26 21:28:52 +0000

    Fixed table name case
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@91 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- doc/sql/insert-users.sql
+++ doc/sql/insert-users.sql
@@ -1,1 +1,1 @@
-insert into user (fullName, loginName, password) values ('Administrator', 'admin', '900150983cd24fb0d6963f7d28e17f72');
+insert into User (fullName, loginName, password) values ('Administrator', 'admin', '900150983cd24fb0d6963f7d28e17f72');

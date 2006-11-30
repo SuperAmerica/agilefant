98671d759275ad64fc195365a47140b292999be4
tvainiok <tvainiok@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2006-11-30 15:43:38 +0000

    UserActionTest, joka käyttää AbstractTransactionalSpringContextTests:ä, tuntuisi toimivan antin run-unit-tests:illä. (Ainakin testikäyttäjä meni kantaan, muuten täytyy vielä jalostaa tuota)
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@104 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- test/unit/fi/hut/soberit/agilefant/web/UserActionTest.java
+++ test/unit/fi/hut/soberit/agilefant/web/UserActionTest.java
@@ -1,6 +1,8 @@
 package fi.hut.soberit.agilefant.web;
 
 import org.springframework.test.AbstractTransactionalSpringContextTests;
+import com.opensymphony.xwork.Action;
+import fi.hut.soberit.agilefant.model.User;
 
 public class UserActionTest extends AbstractTransactionalSpringContextTests {
 	
@@ -8,9 +10,10 @@ public class UserActionTest extends AbstractTransactionalSpringContextTests {
 
 	@Override
 	protected String[] getConfigLocations() {
-		//return new String[]{"WEB-INF/applicationContext*.xml"};
-		return new String[] {"file:conf/applicationContext*.xml"}; 
-//				"file:conf/configuration.properties"};
+		// with Eclipse: (doesn't work yet)
+//		return new String[]{"file:conf/applicationContext*.xml"}; // for testing with ant
+		// works with ant:
+		return new String[]{"file:WEB-INF/applicationContext*.xml"}; // for testing with ant
 	}
 	
 	public void setUserAction(UserAction userAction){
@@ -20,5 +23,18 @@ public class UserActionTest extends AbstractTransactionalSpringContextTests {
 	public void testCreate(){
 		userAction.create();
 		super.assertEquals(0, userAction.getUser().getId());
+		// enpä tiedä, onko nämä seuraavat fiksuja..
+		
+		User u = new User();
+		u.setFullName("Unit TestiKäyttäjä");
+		u.setId(userAction.getUser().getId());
+		u.setPassword("foobar");
+		userAction.setPassword1("foobar");
+		userAction.setPassword2("foobar");
+		userAction.setUser(u);
+		userAction.store();
+		this.setComplete();
+		this.endTransaction();
+//		assertEquals(Action.SUCCESS, userAction.store());
 	}
 }

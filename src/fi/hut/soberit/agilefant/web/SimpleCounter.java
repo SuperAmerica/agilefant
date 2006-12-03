46d90865be8dfe9b9849123c4b5bc55b2d168d79
mpmerila <mpmerila@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2006-12-03 16:53:01 +0000

    Added support to display and refresh JFeeChart images
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@112 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- src/fi/hut/soberit/agilefant/web/SimpleCounter.java
+++ src/fi/hut/soberit/agilefant/web/SimpleCounter.java
@@ -1,8 +1,16 @@
 package fi.hut.soberit.agilefant.web;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 import com.opensymphony.webwork.interceptor.SessionAware;
 import com.opensymphony.xwork.Action;
@@ -49,5 +57,35 @@ public class SimpleCounter extends ActionSupport implements SessionAware {
 		count = new Integer(counter);
 		sessionMap.put(SimpleCounter.COUNTER_PARAM, count);
 		log.info("Counter increased. New value is " + counter); 
+	}
+	
+	public String refreshChart(){
+//		 Create a simple XY chart
+		XYSeries series = new XYSeries("XYGraph");
+		series.add(1, 1);
+		series.add(1, 2);
+		series.add(2, 1);
+		series.add(3, 9);
+		series.add(4, 10);
+//		 Add the series to your data set
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		dataset.addSeries(series);
+//		 Generate the graph
+		JFreeChart chart = ChartFactory.createXYLineChart(
+				"XY Chart", // Title
+				"x-axis", // x-axis Label
+				"y-axis", // y-axis Label
+				dataset, // Dataset
+				PlotOrientation.VERTICAL, // Plot Orientation
+				true, // Show Legend
+				true, // Use tooltips
+				false // Configure chart to generate URLs?
+		);
+		try {
+			ChartUtilities.saveChartAsPNG(new File("./chart.png"), chart, 500, 300);
+		} catch (IOException e) {
+			System.err.println("Problem occurred creating chart.");
+		}
+		return Action.SUCCESS;
 	}
 }

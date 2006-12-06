e2db67bede58a41550af307a48642dcdbf7db0b6
kheleniu <kheleniu@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2006-12-06 18:51:45 +0000

    Added OnlyDateConverter.
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@132 29784df9-99d5-7e28-3e6c-ac1830307ffc

---
+++ src/fi/hut/soberit/agilefant/util/OnlyDateConverter.java
@@ -0,0 +1,43 @@
+package fi.hut.soberit.agilefant.util;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Map;
+
+import com.opensymphony.webwork.util.WebWorkTypeConverter;
+import com.opensymphony.xwork.ActionContext;
+import com.opensymphony.xwork.util.TypeConversionException;
+
+public class OnlyDateConverter extends WebWorkTypeConverter{
+
+	@Override
+	public Object convertFromString(Map context, String[] values, Class toClass) {
+		String value = values[0];
+		
+		DateFormat df = this.getDateFormat();
+		
+		try{
+			return df.parse(value);
+		} catch (ParseException e){
+			throw new TypeConversionException(e.getMessage());
+		}
+	}
+
+	@Override
+	public String convertToString(Map context, Object o) {
+		if (o == null){
+			return null;
+		}
+
+		Date date = (Date)o;
+		
+		DateFormat df = this.getDateFormat();
+		
+		return df.format(date);
+	}
+	
+	protected DateFormat getDateFormat(){
+		return DateFormat.getDateInstance(DateFormat.SHORT, ActionContext.getContext().getLocale());
+	}
+}

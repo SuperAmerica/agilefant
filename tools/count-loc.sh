f117f236236fb0ac19ded2d68609f2889f420334
ekantola <ekantola@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2006-12-09 23:49:30 +0000

    Added ant undeploy-local target; more metrics in count-loc
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@173 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- tools/count-loc.sh
+++ tools/count-loc.sh
@@ -1,7 +1,26 @@
 #! /bin/sh
 
-echo Java code:
-cat `find src test -name '*.java'` | wc
+loc() {
+	echo -n "$1: "
+	shift
+	cat `find "$@"` | wc -l
+}
 
-echo Configuration code:
-cat `find build.* conf -type f` | grep -v '\.svn' | wc
+loc "Java code total" src -name '*.java'
+loc "Database code" src/fi/hut/soberit/agilefant/db -name '*.java'
+loc "Data model code" src/fi/hut/soberit/agilefant/model -name '*.java'
+loc "Security code" src/fi/hut/soberit/agilefant/security -name '*.java'
+loc "Service code" src/fi/hut/soberit/agilefant/service -name '*.java'
+loc "Utility code" src/fi/hut/soberit/agilefant/util -name '*.java'
+loc "Web code" src/fi/hut/soberit/agilefant/web -name '*.java'
+loc "Java tests" test -name '*.java'
+
+echo
+
+loc "Build code" build.*
+loc "Configuration code" conf -type f \! -path '*/.svn*'
+loc "JavaScript code" web/static -name '*.js'
+loc "Dynamic web content total" web/WEB-INF -type f \! -path '*/.svn*'
+loc "Java Servlet Page code" web/WEB-INF -name '*.jsp'
+loc "JSP tag code" web/WEB-INF -name '*.tag'
+loc "JSP TagLib code" web/WEB-INF -name '*.tld'

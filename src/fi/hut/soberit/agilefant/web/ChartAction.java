1a47c64e27658f8812a986d64dd79b2cbe445b29
mpmerila <mpmerila@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2006-12-09 13:58:26 +0000

    First version, might not work correctly
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@164 29784df9-99d5-7e28-3e6c-ac1830307ffc

---
+++ src/fi/hut/soberit/agilefant/web/ChartAction.java
@@ -0,0 +1,207 @@
+package fi.hut.soberit.agilefant.web;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Date;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+import com.opensymphony.xwork.Action;
+import com.opensymphony.xwork.ActionSupport;
+
+import fi.hut.soberit.agilefant.db.BacklogItemDAO;
+import fi.hut.soberit.agilefant.db.DeliverableDAO;
+import fi.hut.soberit.agilefant.db.IterationDAO;
+import fi.hut.soberit.agilefant.db.PerformedWorkDAO;
+import fi.hut.soberit.agilefant.db.TaskDAO;
+import fi.hut.soberit.agilefant.model.AFTime;
+import fi.hut.soberit.agilefant.model.PerformedWork;
+
+
+
+public class ChartAction extends ActionSupport {
+	
+	private byte[] result;
+	private PerformedWork performedWork;
+	private int taskId;
+	private int backlogItemId;
+	private int iterationId;
+	private int deliverableId;
+	private TaskDAO taskDAO;
+	private BacklogItemDAO backlogItemDAO;
+	private IterationDAO iterationDAO;
+	private DeliverableDAO deliverableDAO;
+	private PerformedWorkDAO performedWorkDAO;
+	private Collection<PerformedWork> works;
+	
+	
+	public String execute(){
+//		 Create a time series chart
+		
+		if (taskId > 0){
+			works = performedWorkDAO.getPerformedWork(taskDAO.get(taskId));
+		} else if (backlogItemId > 0){
+			works = performedWorkDAO.getPerformedWork(backlogItemDAO.get(backlogItemId));
+		} else if (iterationId > 0){
+			works = performedWorkDAO.getPerformedWork(iterationDAO.get(iterationId));
+		} else if (deliverableId > 0){
+			works = performedWorkDAO.getPerformedWork(deliverableDAO.get(deliverableId));
+		}
+		TimeSeries pop = new TimeSeries("Workhours", Day.class);
+		Iterator itr = works.iterator();
+
+		
+		while(itr.hasNext()){
+			performedWork = (PerformedWork)itr.next();
+			AFTime effort = performedWork.getEffort();
+			long worktime = 0;
+			long time = effort.getTime();
+			long days = time / AFTime.WORKDAY_IN_MILLIS;
+			time %= AFTime.WORKDAY_IN_MILLIS;
+			
+			long hours = time / AFTime.HOUR_IN_MILLIS;
+			time %= AFTime.HOUR_IN_MILLIS;
+			
+			long minutes = time / AFTime.MINUTE_IN_MILLIS;
+			time %= AFTime.MINUTE_IN_MILLIS;
+			
+			worktime = Math.round(days * 24 + hours + (minutes/60));
+			Date date = performedWork.getCreated();
+			pop.add(new Day(date.getDay(), date.getMonth(), date.getYear()), worktime);
+		}
+		
+		TimeSeriesCollection dataset = new TimeSeriesCollection();
+		dataset.addSeries(pop);
+		JFreeChart chart1 = ChartFactory.createTimeSeriesChart(
+		"Agilefant07 worhours per day",
+		"Date",
+		"Workhours",
+		dataset,
+		true,
+		true,
+		false);
+		XYPlot plot = chart1.getXYPlot();
+		DateAxis axis = (DateAxis) plot.getDomainAxis();
+		axis.setDateFormatOverride(new SimpleDateFormat("yyyy-MM-dd"));	
+		try {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			ChartUtilities.writeChartAsPNG(out, chart1, 500, 300);
+			result = out.toByteArray();		
+		} catch (IOException e) {
+			System.err.println("Problem occurred creating chart.");
+		}
+		return Action.SUCCESS;
+	}
+	
+	public InputStream getInputStream(){
+		return new ByteArrayInputStream(result);
+	}
+
+	public BacklogItemDAO getBacklogItemDAO() {
+		return backlogItemDAO;
+	}
+
+	public void setBacklogItemDAO(BacklogItemDAO backlogItemDAO) {
+		this.backlogItemDAO = backlogItemDAO;
+	}
+
+	public int getBacklogItemId() {
+		return backlogItemId;
+	}
+
+	public void setBacklogItemId(int backlogItemId) {
+		this.backlogItemId = backlogItemId;
+	}
+
+	public DeliverableDAO getDeliverableDAO() {
+		return deliverableDAO;
+	}
+
+	public void setDeliverableDAO(DeliverableDAO deliverableDAO) {
+		this.deliverableDAO = deliverableDAO;
+	}
+
+	public int getDeliverableId() {
+		return deliverableId;
+	}
+
+	public void setDeliverableId(int deliverableId) {
+		this.deliverableId = deliverableId;
+	}
+
+	public IterationDAO getIterationDAO() {
+		return iterationDAO;
+	}
+
+	public void setIterationDAO(IterationDAO iterationDAO) {
+		this.iterationDAO = iterationDAO;
+	}
+
+	public int getIterationId() {
+		return iterationId;
+	}
+
+	public void setIterationId(int iterationId) {
+		this.iterationId = iterationId;
+	}
+
+	public PerformedWork getPerformedWork() {
+		return performedWork;
+	}
+
+	public void setPerformedWork(PerformedWork performedWork) {
+		this.performedWork = performedWork;
+	}
+
+	public PerformedWorkDAO getPerformedWorkDAO() {
+		return performedWorkDAO;
+	}
+
+	public void setPerformedWorkDAO(PerformedWorkDAO performedWorkDAO) {
+		this.performedWorkDAO = performedWorkDAO;
+	}
+
+	public byte[] getResult() {
+		return result;
+	}
+
+	public void setResult(byte[] result) {
+		this.result = result;
+	}
+
+	public TaskDAO getTaskDAO() {
+		return taskDAO;
+	}
+
+	public void setTaskDAO(TaskDAO taskDAO) {
+		this.taskDAO = taskDAO;
+	}
+
+	public int getTaskId() {
+		return taskId;
+	}
+
+	public void setTaskId(int taskId) {
+		this.taskId = taskId;
+	}
+
+	public Collection<PerformedWork> getWorks() {
+		return works;
+	}
+
+	public void setWorks(Collection<PerformedWork> works) {
+		this.works = works;
+	}
+
+}

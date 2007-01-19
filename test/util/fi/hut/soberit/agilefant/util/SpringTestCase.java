23e9fe774b8a9d05ea920f0095eea51cf7aa31bb
tvainiok <tvainiok@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2007-01-19 14:27:40 +0000

    Nyt toimivat myös Eclipsessä. Paitsi UserTest, joka käyttää ExplicitSpringSupport:ia
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@226 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- test/util/fi/hut/soberit/agilefant/util/SpringTestCase.java
+++ test/util/fi/hut/soberit/agilefant/util/SpringTestCase.java
@@ -11,6 +11,7 @@ import java.util.Properties;
 public abstract class SpringTestCase extends AbstractTransactionalSpringContextTests {
 	@Override
 	protected String[] getConfigLocations() {
-		return new String[]{"file:WEB-INF/applicationContext*.xml"};
+		return new String[]{"file:conf/applicationContext.xml", "file:conf/applicationContext-*.xml"};
+		//return new String[]{"file:conf/applicationContext*.xml"}; // ylempi ainakin tuntuu toimivan tutki, josko jotain erroria antin kautta? 
 	}
 }

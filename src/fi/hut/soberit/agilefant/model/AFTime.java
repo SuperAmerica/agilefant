60cefdd4af56b98cfdb3e071df6f7dab08ee9912
mpmerila <mpmerila@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2007-01-29 22:44:41 +0000

    Added DAY_IN_MILLIS variable.
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@278 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- src/fi/hut/soberit/agilefant/model/AFTime.java
+++ src/fi/hut/soberit/agilefant/model/AFTime.java
@@ -30,6 +30,7 @@ public class AFTime extends java.sql.Time {
 	public static long MINUTE_IN_MILLIS = 60 * SECOND_IN_MILLIS;
 	public static long HOUR_IN_MILLIS = 60 * MINUTE_IN_MILLIS;
 	public static long WORKDAY_IN_MILLIS = 8 * HOUR_IN_MILLIS;
+	public static long DAY_IN_MILLIS = 24 * HOUR_IN_MILLIS;
 	
 	public static long WORKDAY_IN_HOURS = WORKDAY_IN_MILLIS / HOUR_IN_MILLIS;
 	

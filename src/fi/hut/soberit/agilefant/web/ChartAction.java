22b32a864c425bef4e77150fc427dad96e432ecd
mpmerila <mpmerila@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2007-02-04 23:22:56 +0000

    Added gantt demo stuff.
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@317 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- src/fi/hut/soberit/agilefant/web/ChartAction.java
+++ src/fi/hut/soberit/agilefant/web/ChartAction.java
@@ -38,7 +38,10 @@ import fi.hut.soberit.agilefant.db.PerformedWorkDAO;
 import fi.hut.soberit.agilefant.db.TaskDAO;
 import fi.hut.soberit.agilefant.model.AFTime;
 import fi.hut.soberit.agilefant.model.PerformedWork;
-
+import org.jfree.data.category.IntervalCategoryDataset;
+import org.jfree.data.gantt.Task;
+import org.jfree.data.gantt.TaskSeries;
+import org.jfree.data.gantt.TaskSeriesCollection;
 
 
 public class ChartAction extends ActionSupport {
@@ -207,9 +210,9 @@ public class ChartAction extends ActionSupport {
 				
 			}	
 		}
-		if(worksum > 0){ // pop the last days hours
+		//if(worksum > 0){ // pop the last days hours
 			estimateSeries.add(new Day(day_last, month_last, year_last), worksum);		
-		}
+		//}
 		
 		dataset.addSeries(estimateSeries);
 		
@@ -488,6 +491,175 @@ public class ChartAction extends ActionSupport {
 	}
 	*/
 	
+	/*---------------------------------------------------------------------*/
+	
+	/**
+	 * A simple demonstration application showing how to create a Gantt chart with multiple bars per
+	 * task. 
+	 *
+	 */
+	public String ganttChart(){
+
+	        TaskSeries s1 = new TaskSeries("Scheduled");
+	        
+	        Task t1 = new Task(
+	            "Write Proposal", date(1, Calendar.APRIL, 2001), date(5, Calendar.APRIL, 2001)
+	        );
+	        t1.setPercentComplete(1.00);
+	        s1.add(t1);
+	        
+	        Task t2 = new Task(
+	            "Obtain Approval", date(9, Calendar.APRIL, 2001), date(9, Calendar.APRIL, 2001)
+	        );
+	        t2.setPercentComplete(1.00);
+	        s1.add(t2);
+
+	        // here is a task split into two subtasks...
+	        Task t3 = new Task(
+	            "Requirements Analysis", 
+	            date(10, Calendar.APRIL, 2001), date(5, Calendar.MAY, 2001)
+	        );
+	        Task st31 = new Task(
+	            "Requirements 1", 
+	            date(10, Calendar.APRIL, 2001), date(25, Calendar.APRIL, 2001)
+	        );
+	        st31.setPercentComplete(1.0);
+	        Task st32 = new Task(
+	            "Requirements 2", 
+	            date(1, Calendar.MAY, 2001), date(5, Calendar.MAY, 2001)
+	        );
+	        st32.setPercentComplete(1.0);
+	        t3.addSubtask(st31);
+	        t3.addSubtask(st32);
+	        s1.add(t3);
+
+	        // and another...
+	        Task t4 = new Task(
+	            "Design Phase", 
+	            date(6, Calendar.MAY, 2001), date(30, Calendar.MAY, 2001)
+	        );
+	        Task st41 = new Task(
+	             "Design 1", 
+	             date(6, Calendar.MAY, 2001), date(10, Calendar.MAY, 2001)
+	        );
+	        st41.setPercentComplete(1.0);
+	        Task st42 = new Task(
+	            "Design 2", 
+	            date(15, Calendar.MAY, 2001), date(20, Calendar.MAY, 2001)
+	        );
+	        st42.setPercentComplete(1.0);
+	        Task st43 = new Task(
+	            "Design 3", 
+	            date(23, Calendar.MAY, 2001), date(30, Calendar.MAY, 2001)
+	        );
+	        st43.setPercentComplete(0.50);
+	        t4.addSubtask(st41);
+	        t4.addSubtask(st42);
+	        t4.addSubtask(st43);
+	        s1.add(t4);
+
+	        Task t5 = new Task(
+	            "Design Signoff", date(2, Calendar.JUNE, 2001), date(2, Calendar.JUNE, 2001)
+	        ); 
+	        s1.add(t5);
+	                        
+	        Task t6 = new Task(
+	            "Alpha Implementation", date(3, Calendar.JUNE, 2001), date(31, Calendar.JULY, 2001)
+	        );
+	        t6.setPercentComplete(0.60);
+	        
+	        s1.add(t6);
+	        
+	        Task t7 = new Task(
+	            "Design Review", date(1, Calendar.AUGUST, 2001), date(8, Calendar.AUGUST, 2001)
+	        );
+	        t7.setPercentComplete(0.0);
+	        s1.add(t7);
+	        
+	        Task t8 = new Task(
+	            "Revised Design Signoff", 
+	            date(10, Calendar.AUGUST, 2001), date(10, Calendar.AUGUST, 2001)
+	        );
+	        t8.setPercentComplete(0.0);
+	        s1.add(t8);
+	        
+	        Task t9 = new Task(
+	            "Beta Implementation", 
+	            date(12, Calendar.AUGUST, 2001), date(12, Calendar.SEPTEMBER, 2001)
+	        );
+	        t9.setPercentComplete(0.0);
+	        s1.add(t9);
+	        
+	        Task t10 = new Task(
+	            "Testing", date(13, Calendar.SEPTEMBER, 2001), date(31, Calendar.OCTOBER, 2001)
+	        );
+	        t10.setPercentComplete(0.0);
+	        s1.add(t10);
+	        
+	        Task t11 = new Task(
+	            "Final Implementation", 
+	            date(1, Calendar.NOVEMBER, 2001), date(15, Calendar.NOVEMBER, 2001)
+	        );
+	        t11.setPercentComplete(0.0);
+	        s1.add(t11);
+	        
+	        Task t12 = new Task(
+	            "Signoff", date(28, Calendar.NOVEMBER, 2001), date(30, Calendar.NOVEMBER, 2001)
+	        );
+	        t12.setPercentComplete(0.0);
+	        s1.add(t12);
+
+	        TaskSeriesCollection collection = new TaskSeriesCollection();
+	        collection.add(s1);
+	        
+	        IntervalCategoryDataset dataset = collection;
+	        
+	        // create the chart...
+	        JFreeChart chart3 = ChartFactory.createGanttChart(
+	            "Gantt Chart Demo",  // chart title
+	            "Task",              // domain axis label
+	            "Date",              // range axis label
+	            dataset,             // data
+	            true,                // include legend
+	            true,                // tooltips
+	            false                // urls
+	        );
+	        CategoryPlot plot = (CategoryPlot) chart3.getPlot();
+	        //      plot.getDomainAxis().setMaxCategoryLabelWidthRatio(10.0f);
+	        CategoryItemRenderer renderer = plot.getRenderer();
+	        renderer.setSeriesPaint(0, Color.blue);
+	        
+	        try {
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				ChartUtilities.writeChartAsPNG(out, chart3, 500, 300);
+				result = out.toByteArray();		
+			} catch (IOException e) {
+				System.err.println("Problem occurred creating chart.");
+			}
+			return Action.SUCCESS;
+		}	
+
+	    /**
+	     * Utility method for creating <code>Date</code> objects.
+	     *
+	     * @param day  the date.
+	     * @param month  the month.
+	     * @param year  the year.
+	     *
+	     * @return a date.
+	     */
+	    private static Date date(final int day, final int month, final int year) {
+
+	        Calendar calendar = Calendar.getInstance();
+	        calendar.set(year, month, day);
+	        Date result = calendar.getTime();
+	        return result;
+
+	    }
+	
+	
+	/*---------------------------------------------------------------------------*/
+
 	public InputStream getInputStream(){
 		return new ByteArrayInputStream(result);
 	}

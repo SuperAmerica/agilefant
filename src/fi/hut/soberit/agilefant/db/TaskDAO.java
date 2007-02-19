b0999929a144e03a7ac6c70dedfed9917833e291
tiaijala <tiaijala@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2007-02-19 22:16:34 +0000

    Added some comments, implemented gettings tasks by status and bli.
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@387 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- src/fi/hut/soberit/agilefant/db/TaskDAO.java
+++ src/fi/hut/soberit/agilefant/db/TaskDAO.java
@@ -1,7 +1,17 @@
 package fi.hut.soberit.agilefant.db;
 
+import java.util.Collection;
+
+import fi.hut.soberit.agilefant.model.BacklogItem;
 import fi.hut.soberit.agilefant.model.Task;
+import fi.hut.soberit.agilefant.model.TaskStatus;
 
+/**
+ * Interface for a DAO of a Task.
+ * 
+ * @see GenericDAO
+ */
 public interface TaskDAO extends GenericDAO<Task> {
-
+	
+	public Collection<Task> getTasksByStatusAndBacklogItem(BacklogItem bli, TaskStatus[] statuses);
 }
--- src/fi/hut/soberit/agilefant/db/hibernate/TaskDAOHibernate.java
+++ src/fi/hut/soberit/agilefant/db/hibernate/TaskDAOHibernate.java
@@ -1,11 +1,46 @@
 package fi.hut.soberit.agilefant.db.hibernate;
 
-import fi.hut.soberit.agilefant.db.TaskDAO;
-import fi.hut.soberit.agilefant.model.Task;
+import java.util.Collection;
 
+import fi.hut.soberit.agilefant.db.TaskDAO;
+import fi.hut.soberit.agilefant.model.BacklogItem;
+import fi.hut.soberit.agilefant.model.Task;
+import fi.hut.soberit.agilefant.model.TaskStatus;
+
+/**
+ * Hibernate implementation of TaskDAO interface using GenericDAOHibernate.
+ */
 public class TaskDAOHibernate extends GenericDAOHibernate<Task> implements TaskDAO {
 
 	public TaskDAOHibernate(){
 		super(Task.class);
 	}
+	
+	/** {@inheritDoc} */
+	@SuppressWarnings("unchecked")
+	public Collection<Task> getTasksByStatusAndBacklogItem(BacklogItem bli, TaskStatus[] statuses) {
+		String[] ids = new String[statuses.length + 1];
+		Object[] values = new Object[statuses.length + 1];
+		
+		ids[0] = "bliid";
+		values[0] = bli.getId();
+		
+		String query = "from Task t where t.backlogItem.id = :bliid and ( ";
+		boolean prev = false;
+		int i = 0;
+		for(TaskStatus status : statuses) {
+			if(prev) query += " or ";
+			query += "( t.status = :status" + i + " )";  
+			ids[i+1] = "status" + i;
+			values[i+1] = status;
+			i++;  
+		}
+		
+		query += " )";
+		
+		return (Collection<Task>)super.getHibernateTemplate().findByNamedParam(
+				query,
+				ids, 
+				values);
+	}	
 }

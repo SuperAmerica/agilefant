7d93d31e8829ee06386af09a8e9a905c4c2d075e
rtammisa <rtammisa@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2008-05-28 14:13:04 +0000

    HourEntryDAOHibernate getEntriesByTarget()-method implemented.
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@1092 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- src/fi/hut/soberit/agilefant/db/hibernate/HourEntryDAOHibernate.java
+++ src/fi/hut/soberit/agilefant/db/hibernate/HourEntryDAOHibernate.java
@@ -1,11 +1,18 @@
 package fi.hut.soberit.agilefant.db.hibernate;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Expression;
+import org.hibernate.criterion.Restrictions;
 
 import fi.hut.soberit.agilefant.db.HourEntryDAO;
 import fi.hut.soberit.agilefant.model.Backlog;
 import fi.hut.soberit.agilefant.model.HourEntry;
+import fi.hut.soberit.agilefant.model.Setting;
 import fi.hut.soberit.agilefant.model.TimesheetLoggable;
 
 public class HourEntryDAOHibernate extends GenericDAOHibernate<HourEntry> implements
@@ -14,15 +21,28 @@ public class HourEntryDAOHibernate extends GenericDAOHibernate<HourEntry> implem
     protected HourEntryDAOHibernate() {
         super(HourEntry.class);
     }
-
-    public List<HourEntry> getEntriesByTarget(TimesheetLoggable target) {
-        // TODO Auto-generated method stub
-        return null;
+    
+    /**
+     * {@inheritDoc}
+     */
+    @SuppressWarnings("all")
+    public List<HourEntry> getEntriesByTarget(TimesheetLoggable target) {    
+        DetachedCriteria criteria = DetachedCriteria.forClass(this
+                .getPersistentClass());
+        criteria.add(Restrictions.eq("targetType", target.timesheetType()));
+        criteria.add(Restrictions.eq("targetId", target.getId()));
+        
+        return super.getHibernateTemplate().findByCriteria(criteria);       
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @SuppressWarnings("all")
     public Map<Integer, Integer> getSumsByBacklog(Backlog backlog) {
-        // TODO Auto-generated method stub
-        return null;
+        Map<Integer, Integer> sums = new HashMap<Integer, Integer>();
+        // TODO: blaah.
+        return sums;
     }
 
 

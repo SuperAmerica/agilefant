79133394d6495455bf3c0a4fe5a01e4cc6144fd4
rtammisa <rtammisa@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2008-06-12 14:05:04 +0000

    Added data to the hour entries
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@1242 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- test/fi/hut/soberit/agilefant/business/TimesheetBusinessTest.java
+++ test/fi/hut/soberit/agilefant/business/TimesheetBusinessTest.java
@@ -5,9 +5,13 @@ import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
+import fi.hut.soberit.agilefant.db.BacklogDAO;
 import fi.hut.soberit.agilefant.db.UserDAO;
+import fi.hut.soberit.agilefant.model.AFTime;
 import fi.hut.soberit.agilefant.model.Backlog;
 import fi.hut.soberit.agilefant.model.BacklogHourEntry;
 import fi.hut.soberit.agilefant.model.BacklogItem;
@@ -25,15 +29,17 @@ public class TimesheetBusinessTest extends TestCase {
     private Iteration iteration1, iteration2, iteration3;
     private ArrayList<BacklogItem> bliList;
     private ArrayList<HourEntry> heList; 
-    private User user1, user2, user3;
+    private User user1, user2;
     private HourEntryBusiness hourEntryBusiness;
     private UserBusiness userBusiness;
     private UserDAO userDAO;
+    private BacklogDAO backlogDAO;
     
     public void setUp() {
         hourEntryBusiness = createMock(HourEntryBusiness.class);
         userBusiness = createMock(UserBusiness.class);
         userDAO = createMock(UserDAO.class);
+        backlogDAO = createMock(BacklogDAO.class);
         product1 = setUpProduct(1);
         product2 = setUpProduct(2);
         project1 = setUpProject(product2, 3);
@@ -41,6 +47,7 @@ public class TimesheetBusinessTest extends TestCase {
         iteration1 = setUpIteration(project1, 5);
         iteration2 = setUpIteration(project1, 6);
         iteration3 = setUpIteration(project1, 7);
+        createUsers();
         createBacklogItems();
         createHourEntries();
     }
@@ -49,12 +56,18 @@ public class TimesheetBusinessTest extends TestCase {
         
     }
     
+    /**
+     * Inserts all BacklogHourEntries to project2.
+     */
     private void insertAllBacklogHourEntries() {
         insertBacklogHourEntry(heList.get(11), project2);
         insertBacklogHourEntry(heList.get(12), project2);
         insertBacklogHourEntry(heList.get(13), project2);
     }
     
+    /**
+     * Inserts all BacklogItemHourEntries to BLIs.
+     */
     private void insertAllBacklogItemHourEntries() {
         insertBacklogItemHourEntry(heList.get(0), bliList.get(0));
         insertBacklogItemHourEntry(heList.get(1), bliList.get(1));
@@ -69,18 +82,24 @@ public class TimesheetBusinessTest extends TestCase {
         insertBacklogItemHourEntry(heList.get(10), bliList.get(8));
     }
     
+    /**
+     * Inserts the BacklogItemHourEntry to the specified BLI.
+     */
     private void insertBacklogItemHourEntry(HourEntry he, BacklogItem bli) {
         BacklogItemHourEntry blihe = (BacklogItemHourEntry) he;
         blihe.setBacklogItem(bli);
     }
     
+    /**
+     * Inserts the BacklogHourEntry to the specified backlog.
+     */
     private void insertBacklogHourEntry(HourEntry he, Backlog backlog) {
         BacklogHourEntry blhe = (BacklogHourEntry) he;
         blhe.setBacklog(backlog);
     }
     
     /**
-     * Create a new product with the given id.
+     * Creates a new product with the given id.
      */
     private Product setUpProduct(int id) {
         Product prod = new Product();
@@ -112,7 +131,7 @@ public class TimesheetBusinessTest extends TestCase {
     }
     
     /**
-     * Creates a new project with the given id and link it to a product.
+     * Creates a new project with the given id and links it to a product.
      */
     private Project setUpProject(Product prod, int id) {
         Project proj = new Project();
@@ -123,7 +142,7 @@ public class TimesheetBusinessTest extends TestCase {
     }
     
     /**
-     * Creates a new iteration with the given id and link it to a project.
+     * Creates a new iteration with the given id and links it to a project.
      */
     private Iteration setUpIteration(Project proj, int id) {
         Iteration iter = new Iteration();
@@ -135,7 +154,7 @@ public class TimesheetBusinessTest extends TestCase {
     
     /**
      * Creates all the needed HourEntries, gives them ids and inserts
-     * them to the heList.
+     * them to heList.
      */
     private void createHourEntries() {
         heList = new ArrayList<HourEntry>();
@@ -152,11 +171,55 @@ public class TimesheetBusinessTest extends TestCase {
         }
         insertAllBacklogItemHourEntries();
         insertAllBacklogHourEntries();
+        fillAllHourEntries();
+    }
+    
+    /**
+     * Fills all the hour entries with data.
+     */
+    private void fillAllHourEntries() {
+        fillHourEntry(heList.get(0),  "1-1-2006 13:00",   60,     user1);
+        fillHourEntry(heList.get(1),  "1-1-2007 13:00",   120,    user2);
+        fillHourEntry(heList.get(2),  "1-1-2008 13:00",   240,    user1);
+        fillHourEntry(heList.get(3),  "10-5-2008 13:00",  480,    user2);
+        fillHourEntry(heList.get(4),  "20-5-2008 13:00",  0,      user1);
+        fillHourEntry(heList.get(5),  "1-6-2008 13:00",   1920,   user2);
+        fillHourEntry(heList.get(6),  "5-6-2008 13:00",   3840,   user1);
+        fillHourEntry(heList.get(7),  "10-6-2008 13:00",  7680,   user2);
+        fillHourEntry(heList.get(8),  "11-6-2008 13:00",  15360,  user1);
+        fillHourEntry(heList.get(9),  "12-6-2008 13:00",  30720,  user2);
+        fillHourEntry(heList.get(10), "13-6-2008 13:00",  61440,  user1);
+        fillHourEntry(heList.get(11), "10-11-2008 13:00", 122880, user2);
+        fillHourEntry(heList.get(12), "1-1-2050 13:00",   245760, user1);
+        fillHourEntry(heList.get(13), "5-3-1980 13:00",   960,    user2);
+    }
+    
+    /**
+     * Sets the hour entry's date, time and user.
+     */
+    private void fillHourEntry(HourEntry he, String date, int time, User user) {
+        he.setTimeSpent(new AFTime(time));
+        he.setUser(user);
+        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+        df.setLenient(true);
+        try {
+            he.setDate(df.parse(date));
+        } catch (ParseException pe) {
+            
+        }
+    }
+    
+    private void createUsers() {
+        user1 = new User();
+        user1.setId(1);
+        user2 = new User();
+        user2.setId(2);
+        
     }
     
     /**
      * Creates all needed BacklogItems, gives them ids and inserts
-     * them to the bliList.
+     * them to bliList.
      */
     private void createBacklogItems() {
         bliList = new ArrayList<BacklogItem>();

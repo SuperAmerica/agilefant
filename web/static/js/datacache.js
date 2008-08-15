c8980b211584dcbdbf5cff196e0e0c099a84a10b
ppekkane <ppekkane@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2008-08-15 10:55:49 +0000

    + added peek method
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@1422 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- web/static/js/datacache.js
+++ web/static/js/datacache.js
@@ -3,30 +3,37 @@ var dataCacheClass = function() {
     this.fetchURLs = {
         "allUsers": "getUserJSON.action",
         "allTeams": "getTeamJSON.action"
-    }
-}
-
-dataCacheClass.prototype.get = function(element, options) {
-    var me = this;
-    var settings = {
-		url: me.fetchURLs[element],
-		async: false,
-		dataType: 'json',
-		type: 'POST',
-		success: function(data, status) {
-		    me.put(element, data);
-        }
     };
-    jQuery.extend(settings, options);
-    if (this[element] == null) {
-        this[element] = [];
-        $.ajax(settings);
-    }
-    return this[element];
+    this.data = {};
 }
 
-dataCacheClass.prototype.put = function(element, value) {
-    this[element] = value;
-}
+dataCacheClass.prototype = {
+	get: function(element, options) {
+	    var me = this;
+	    var settings = {
+			url: me.fetchURLs[element],
+			async: false,
+			dataType: 'json',
+			type: 'POST',
+			success: function(data, status) {
+			    me.put(element, data);
+	        }
+	    };
+	    jQuery.extend(settings, options);
+	    if (this.data[element] == null) {
+	        this.data[element] = [];
+	        $.ajax(settings);
+	    }
+	    return this.data[element];
+	},
+	
+	put: function(element, value) {
+	    this.data[element] = value;
+	},
+	
+	peek: function(element) {
+		return this.data[element];
+	}
+};
 
 var jsonDataCache = new dataCacheClass();

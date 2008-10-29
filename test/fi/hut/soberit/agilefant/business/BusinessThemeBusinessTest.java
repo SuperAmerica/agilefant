5cd2cb022a562eba85446047b0993533e200c00b
jruutiai <jruutiai@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2008-10-29 07:56:45 +0000

    added test for BusinessThemeBusiness.getBacklogItemActiveOrSelectedThemes
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@1595 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- test/fi/hut/soberit/agilefant/business/BusinessThemeBusinessTest.java
+++ test/fi/hut/soberit/agilefant/business/BusinessThemeBusinessTest.java
@@ -16,24 +16,37 @@ import fi.hut.soberit.agilefant.business.impl.BusinessThemeBusinessImpl;
 import fi.hut.soberit.agilefant.db.BacklogDAO;
 import fi.hut.soberit.agilefant.db.BacklogItemDAO;
 import fi.hut.soberit.agilefant.db.BusinessThemeDAO;
+import fi.hut.soberit.agilefant.db.IterationGoalDAO;
+import fi.hut.soberit.agilefant.db.ProductDAO;
+import fi.hut.soberit.agilefant.db.ProjectDAO;
+import fi.hut.soberit.agilefant.db.TaskDAO;
+import fi.hut.soberit.agilefant.db.UserDAO;
 import fi.hut.soberit.agilefant.exception.ObjectNotFoundException;
+import fi.hut.soberit.agilefant.model.AFTime;
+import fi.hut.soberit.agilefant.model.Backlog;
 import fi.hut.soberit.agilefant.model.BacklogItem;
 import fi.hut.soberit.agilefant.model.BacklogThemeBinding;
 import fi.hut.soberit.agilefant.model.BusinessTheme;
 import fi.hut.soberit.agilefant.model.Iteration;
+import fi.hut.soberit.agilefant.model.IterationGoal;
+import fi.hut.soberit.agilefant.model.Priority;
 import fi.hut.soberit.agilefant.model.Product;
 import fi.hut.soberit.agilefant.model.Project;
+import fi.hut.soberit.agilefant.model.State;
+import fi.hut.soberit.agilefant.model.Task;
+import fi.hut.soberit.agilefant.model.User;
+import fi.hut.soberit.agilefant.util.SpringTestCase;
 import junit.framework.TestCase;
 
 public class BusinessThemeBusinessTest extends TestCase {
-    
+
     private BusinessThemeBusinessImpl themeBusiness = new BusinessThemeBusinessImpl();
     private BusinessThemeDAO themeDAO;
-    
+
     public void testDeleteBusinessThemeWithBlis() {
         themeDAO = createMock(BusinessThemeDAO.class);
         themeBusiness.setBusinessThemeDAO(themeDAO);
-        
+
         int bliId1 = 100;
         BacklogItem bli1 = new BacklogItem();
         bli1.setId(bliId1);
@@ -51,35 +64,35 @@ public class BusinessThemeBusinessTest extends TestCase {
         items.add(bli1);
         theme1.setBacklogItems(items);
         bli1.getBusinessThemes().add(theme1);
-        
-        
+
+
         // Record expected behavior
         expect(themeDAO.get(themeId1)).andReturn(theme1);
         themeDAO.remove(themeId1);
         replay(themeDAO);
-        
+
         // run method under test
         try {
             themeBusiness.delete(themeId1);
         } catch (ObjectNotFoundException onfe) {
             fail();
         }
-        
+
         // No themes in bli.
         assertEquals(0, bli1.getBusinessThemes().size());
-        
+
         // verify behavior
         verify(themeDAO);
-        
+
     }
-    
+
     /**
      * Add theme binding to iteration, which has no themes associated 
      * with itself or with the parent project.
      */
     public void testAddThemeToBacklog_IterationNew() {
 
-         //setup backlogs
+        //setup backlogs
         Project proj = new Project();
         Iteration iter = new Iteration();
         proj.setId(1);
@@ -102,77 +115,77 @@ public class BusinessThemeBusinessTest extends TestCase {
         expect(themeDAO.get(1)).andReturn(theme1).atLeastOnce();
         //expect one save call - business creates the binding, so only expect correct type
         themeDAO.saveOrUpdateBacklogThemeBinding(EasyMock.isA(BacklogThemeBinding.class));
-        
+
         replay(themeDAO);
         replay(backlogDAO);
-        
+
         //run
         themeBusiness.addOrUpdateThemeToBacklog(theme1.getId(), iter.getId(), "0h");
         verify(themeDAO);
         verify(backlogDAO);
-        
-        
+
+
     }
-    
+
     public void testAddThemeToBacklog_IterationExisting_Formats() {
 
         //setup backlogs
-       Project proj = new Project();
-       Iteration iter = new Iteration();
-       proj.setId(1);
-       iter.setId(2);
-       List<Iteration> iterations = new ArrayList<Iteration>();
-       iterations.add(iter);
-       proj.setIterations(iterations);
-       iter.setProject(proj);
-       //setup theme
-       BusinessTheme theme1 = new BusinessTheme();
-       BacklogThemeBinding bind = new BacklogThemeBinding();
-       bind.setBacklog(iter);
-       bind.setBusinessTheme(theme1);
-       List<BacklogThemeBinding> bindings = new ArrayList<BacklogThemeBinding>();
-       bindings.add(bind);
-       theme1.setBacklogBindings(bindings);
-       iter.setBusinessThemeBindings(bindings);
-       theme1.setId(1);
-       //setup services
-       BusinessThemeBusinessImpl themeBusiness = new BusinessThemeBusinessImpl();
-       BusinessThemeDAO themeDAO = EasyMock.createMock(BusinessThemeDAO.class);
-       BacklogDAO backlogDAO = EasyMock.createMock(BacklogDAO.class);
-       themeBusiness.setBacklogDAO(backlogDAO);
-       themeBusiness.setBusinessThemeDAO(themeDAO);
-       //expected DAO calls
-       expect(backlogDAO.get(2)).andReturn(iter).atLeastOnce();
-       expect(themeDAO.get(1)).andReturn(theme1).atLeastOnce();
+        Project proj = new Project();
+        Iteration iter = new Iteration();
+        proj.setId(1);
+        iter.setId(2);
+        List<Iteration> iterations = new ArrayList<Iteration>();
+        iterations.add(iter);
+        proj.setIterations(iterations);
+        iter.setProject(proj);
+        //setup theme
+        BusinessTheme theme1 = new BusinessTheme();
+        BacklogThemeBinding bind = new BacklogThemeBinding();
+        bind.setBacklog(iter);
+        bind.setBusinessTheme(theme1);
+        List<BacklogThemeBinding> bindings = new ArrayList<BacklogThemeBinding>();
+        bindings.add(bind);
+        theme1.setBacklogBindings(bindings);
+        iter.setBusinessThemeBindings(bindings);
+        theme1.setId(1);
+        //setup services
+        BusinessThemeBusinessImpl themeBusiness = new BusinessThemeBusinessImpl();
+        BusinessThemeDAO themeDAO = EasyMock.createMock(BusinessThemeDAO.class);
+        BacklogDAO backlogDAO = EasyMock.createMock(BacklogDAO.class);
+        themeBusiness.setBacklogDAO(backlogDAO);
+        themeBusiness.setBusinessThemeDAO(themeDAO);
+        //expected DAO calls
+        expect(backlogDAO.get(2)).andReturn(iter).atLeastOnce();
+        expect(themeDAO.get(1)).andReturn(theme1).atLeastOnce();
 
 
-       themeDAO.saveOrUpdateBacklogThemeBinding(bind);
-       themeDAO.saveOrUpdateBacklogThemeBinding(bind);
-       themeDAO.saveOrUpdateBacklogThemeBinding(bind);
-       themeDAO.saveOrUpdateBacklogThemeBinding(bind);
-       themeDAO.saveOrUpdateBacklogThemeBinding(bind);
-       replay(themeDAO);
-       replay(backlogDAO);
-       
-       //run
-       themeBusiness.addOrUpdateThemeToBacklog(theme1.getId(), iter.getId(), "0h");
-       assertEquals(bind.isRelativeBinding(), false);
-       assertEquals(bind.getFixedSize().getTime(),0);
-       themeBusiness.addOrUpdateThemeToBacklog(theme1.getId(), iter.getId(), "0%");
-       assertEquals(bind.isRelativeBinding(), true);
-       themeBusiness.addOrUpdateThemeToBacklog(theme1.getId(), iter.getId(), "10%");
-       assertEquals(bind.isRelativeBinding(), true);
-       themeBusiness.addOrUpdateThemeToBacklog(theme1.getId(), iter.getId(), "1h");
-       assertEquals(bind.getFixedSize().getTime(),3600);
-       assertEquals(bind.isRelativeBinding(), false);
-       themeBusiness.addOrUpdateThemeToBacklog(theme1.getId(), iter.getId(), "1h 30min");
-       assertEquals(bind.isRelativeBinding(), false);
-       assertEquals(bind.getFixedSize().getTime(),5400);
-       verify(themeDAO);
-       verify(backlogDAO);
-   
-   }
-    
+        themeDAO.saveOrUpdateBacklogThemeBinding(bind);
+        themeDAO.saveOrUpdateBacklogThemeBinding(bind);
+        themeDAO.saveOrUpdateBacklogThemeBinding(bind);
+        themeDAO.saveOrUpdateBacklogThemeBinding(bind);
+        themeDAO.saveOrUpdateBacklogThemeBinding(bind);
+        replay(themeDAO);
+        replay(backlogDAO);
+
+        //run
+        themeBusiness.addOrUpdateThemeToBacklog(theme1.getId(), iter.getId(), "0h");
+        assertEquals(bind.isRelativeBinding(), false);
+        assertEquals(bind.getFixedSize().getTime(),0);
+        themeBusiness.addOrUpdateThemeToBacklog(theme1.getId(), iter.getId(), "0%");
+        assertEquals(bind.isRelativeBinding(), true);
+        themeBusiness.addOrUpdateThemeToBacklog(theme1.getId(), iter.getId(), "10%");
+        assertEquals(bind.isRelativeBinding(), true);
+        themeBusiness.addOrUpdateThemeToBacklog(theme1.getId(), iter.getId(), "1h");
+        assertEquals(bind.getFixedSize().getTime(),3600);
+        assertEquals(bind.isRelativeBinding(), false);
+        themeBusiness.addOrUpdateThemeToBacklog(theme1.getId(), iter.getId(), "1h 30min");
+        assertEquals(bind.isRelativeBinding(), false);
+        assertEquals(bind.getFixedSize().getTime(),5400);
+        verify(themeDAO);
+        verify(backlogDAO);
+
+    }
+
     /**
      * project has the theme that is being added to the iteration
      */
@@ -208,7 +221,7 @@ public class BusinessThemeBusinessTest extends TestCase {
         expect(themeDAO.get(1)).andReturn(theme1).atLeastOnce();
         themeDAO.saveOrUpdateBacklogThemeBinding(EasyMock.isA(BacklogThemeBinding.class));
         themeDAO.removeBacklogThemeBinding(bind);
-        
+
         replay(themeDAO);
         replay(backlogDAO);
         themeBusiness.addOrUpdateThemeToBacklog(theme1.getId(), iter.getId(), "0h");
@@ -239,18 +252,18 @@ public class BusinessThemeBusinessTest extends TestCase {
         expect(themeDAO.get(1)).andReturn(theme1).atLeastOnce();
         //expect one save call - business creates the binding, so only expect correct type
         themeDAO.saveOrUpdateBacklogThemeBinding(EasyMock.isA(BacklogThemeBinding.class));
-        
+
         replay(themeDAO);
         replay(backlogDAO);
-        
+
         //run
         themeBusiness.addOrUpdateThemeToBacklog(theme1.getId(), proj.getId(), "0h");
         verify(themeDAO);
         verify(backlogDAO);
-        
-     
+
+
     }
-    
+
     public void testAddThemeToBacklog_ProjectIteration() {
         //setup backlogs
         Project proj = new Project();
@@ -282,7 +295,7 @@ public class BusinessThemeBusinessTest extends TestCase {
         //expected DAO calls
         expect(backlogDAO.get(1)).andReturn(proj).atLeastOnce();
         expect(themeDAO.get(1)).andReturn(theme1).atLeastOnce();
-        
+
         replay(themeDAO);
         replay(backlogDAO);
         //call should save the theme binding, as project's iteration has the theme
@@ -290,7 +303,7 @@ public class BusinessThemeBusinessTest extends TestCase {
         verify(themeDAO);
         verify(backlogDAO);
     }
-    
+
     public void testAddThemeToBacklog_duplicates() {
         Project proj = new Project();
         proj.setBusinessThemeBindings(new ArrayList<BacklogThemeBinding>());
@@ -307,16 +320,16 @@ public class BusinessThemeBusinessTest extends TestCase {
         expect(backlogDAO.get(1)).andReturn(proj);
         expect(themeDAO.get(1)).andReturn(theme1);
         themeDAO.saveOrUpdateBacklogThemeBinding(EasyMock.isA(BacklogThemeBinding.class));
-  
+
         replay(backlogDAO);
         replay(themeDAO);
-        
+
         int[] themeIds = {1,1,1,1};
         String[] alloc = {"0","0","0","0"};
         themeBusiness.multipleAddOrUpdateThemeToBacklog(themeIds, 1, alloc);
         verify(backlogDAO);
         verify(themeDAO);
-        
+
     }   
 
     public void testAddMultipleThemesToBacklogItem() {
@@ -338,7 +351,7 @@ public class BusinessThemeBusinessTest extends TestCase {
         bliDAO.store(bli);
         expect(themeDAO.get(2)).andReturn(theme2);
         expect(themeDAO.get(3)).andReturn(theme3);
-        
+
         replay(bliBus);
         replay(themeDAO);
         replay(bliDAO);
@@ -348,24 +361,111 @@ public class BusinessThemeBusinessTest extends TestCase {
         verify(themeDAO);
         verify(bliBus);
         verify(bliDAO);
+
+    }
+
+    public void testGetActiveOrSelecterThemes() {
+        themeDAO = createMock(BusinessThemeDAO.class);
+
+        themeBusiness.setBusinessThemeDAO(themeDAO);
+
+        BacklogDAO backlogDAO = createMock(BacklogDAO.class);
+        BacklogItemDAO backlogItemDAO = createMock(BacklogItemDAO.class);
+        ProductDAO productDAO = createMock(ProductDAO.class);
+
+        themeBusiness.setBacklogDAO(backlogDAO);
+        themeBusiness.setBacklogItemDAO(backlogItemDAO);
+        themeBusiness.setProductDAO(productDAO);
         
+        //create product and project
+        Product product = new Product();
+        product.setName("Test Product");
+
+        //int productId = (Integer) productDAO.create(product);
+        //product = productDAO.get(productId);
+
+        // create iteration and bli
+        //Backlog backlog = new Iteration();
+        Backlog backlog = product;
+        //int backlogId = (Integer) backlogDAO.create(backlog);
+        //backlog = backlogDAO.get(backlogId);
+        BacklogItem bli = new BacklogItem();
+        bli.setBacklog(backlog);
+        //int bliId = (Integer) backlogItemDAO.create(bli);
+        //bli = backlogItemDAO.get(bliId);
+        backlog.getBacklogItems().add(bli);
+
+        // set bli fields
+        bli.setName("Test Name");
+        bli.setDescription("Test Description");
+        bli.setPriority(Priority.BLOCKER);
+        bli.setOriginalEstimate(new AFTime("2h 15min"));
+        bli.setState(State.BLOCKED);
+        bli.setEffortLeft(new AFTime("2h 15min"));
+
+
+        int bliId1 = 100;
+        
+        int prodId = 20;
+        Product prod = new Product();
+        prod.setId(prodId);
+        int themeId1 = 101;
+        BusinessTheme theme1 = new BusinessTheme();
+        theme1.setId(themeId1);
+        theme1.setProduct(prod);
+        theme1.setName("foo");
+        theme1.setDescription("");
+        theme1.setActive(true);
+        int themeId2 = 102;
+        BusinessTheme theme2 = new BusinessTheme();
+        theme2.setId(themeId2);
+        theme2.setProduct(prod);
+        theme2.setName("foo2");
+        theme2.setDescription("");
+        theme2.setActive(false);
+        Set<BusinessTheme> productThemes = new HashSet<BusinessTheme>();
+        productThemes.add(theme1);
+        productThemes.add(theme2);
+        Set<BusinessTheme> itemThemes = new HashSet<BusinessTheme>();
+        itemThemes.add(theme2);
+        product.setBusinessThemes(productThemes);
+        bli.setBusinessThemes(itemThemes);
+
+        expect(backlogItemDAO.get(0)).andReturn(bli);
+        expect(backlogItemDAO.get(0)).andReturn(bli);
+        expect(backlogItemDAO.get(0)).andReturn(bli);
+        replay(backlogItemDAO);
+        assertEquals(2, themeBusiness.getBacklogItemActiveOrSelectedThemes(0).size());
+        theme1.setActive(false);
+        assertEquals(1, themeBusiness.getBacklogItemActiveOrSelectedThemes(0).size());
+        BusinessTheme theme3 = new BusinessTheme();
+        int themeId3 = 103;
+        theme3.setId(themeId3);
+        theme3.setProduct(prod);
+        theme3.setName("foo3");
+        theme3.setDescription("");
+        theme3.setActive(true);
+        productThemes.add(theme3);
+        assertEquals(2, themeBusiness.getBacklogItemActiveOrSelectedThemes(0).size());
+        
+        verify(backlogItemDAO);
     }
     /*
     public void testStoreBusinessTheme() {
         themeDAO = createMock(BusinessThemeDAO.class);
         themeBusiness.setBusinessThemeDAO(themeDAO);
-        
+
         int themeId1 = 101;
         BusinessTheme theme1 = new BusinessTheme();
         theme1.setId(themeId1);
         theme1.setName("foo");
         theme1.setDescription("");         
-        
+
         // Record expected behavior
         expect(themeDAO.get(themeId1)).andReturn(theme1);        
         themeDAO.store(theme1);        
         replay(themeDAO);
-        
+
         // run method under test
         try {
             themeBusiness.store(themeId1, theme1);           
@@ -374,10 +474,10 @@ public class BusinessThemeBusinessTest extends TestCase {
         } catch (Exception e) {
             fail();
         }                        
-               
+
         // verify behavior
         verify(themeDAO);
-        
+
     }
-*/
+     */
 }

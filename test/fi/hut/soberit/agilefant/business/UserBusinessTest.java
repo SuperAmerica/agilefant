b96debc5e92b29969bf1668318204d0be77e9154
hpulkkin <hpulkkin@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2008-11-13 20:14:49 +0000

    + Added an unit test testhasUserCreatedItems()
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@1640 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- test/fi/hut/soberit/agilefant/business/UserBusinessTest.java
+++ test/fi/hut/soberit/agilefant/business/UserBusinessTest.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import com.opensymphony.xwork.Action;
+
 import fi.hut.soberit.agilefant.db.BacklogItemDAO;
 import fi.hut.soberit.agilefant.db.ProductDAO;
 import fi.hut.soberit.agilefant.db.TaskDAO;
@@ -202,4 +204,25 @@ public class UserBusinessTest extends SpringTestCase {
     protected void onTearDownInTransaction() throws Exception {
         testUtility.clearDBStack();
     }
+    
+    public void testhasUserCreatedItems() {
+        
+        User user = userDAO.get(testUtility.createUser("test0", "test"));
+        SecurityUtil.setLoggedUser(user);
+ 
+        assertFalse(userBusiness.hasUserCreatedItems(user));
+        
+        // Create the backlog items
+        int productId = testUtility.createProduct("Test product");
+        Product product = productDAO.get(productId);
+
+        int bli1Id = testUtility.createBacklogItem(
+                "Backlog item with not started state", product);
+        int bli2Id = testUtility.createBacklogItem(
+                "Backlog item with started state", product);
+        BacklogItem bli1 = backlogItemDAO.get(bli1Id);
+        bli1.setCreator(user);
+        
+        assertTrue(userBusiness.hasUserCreatedItems(user));    
+    }   
 }

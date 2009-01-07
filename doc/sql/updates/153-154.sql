dfcb0fa4682df3c50043e245814285b5000ed6cc
rjokelai <rjokelai@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-01-07 10:37:25 +0000

    + Added semicolons and a newline
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@1700 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- doc/sql/updates/153-154.sql
+++ doc/sql/updates/153-154.sql
@@ -1,2 +1,2 @@
-alter table backlogitem add column parentBli_id integer
-alter table backlogitem add index FK655CD5909392F6DD (parentBli_id), add constraint FK655CD5909392F6DD foreign key (parentBli_id) references backlogitem (id)
+alter table backlogitem add column parentBli_id integer;
+alter table backlogitem add index FK655CD5909392F6DD (parentBli_id), add constraint FK655CD5909392F6DD foreign key (parentBli_id) references backlogitem (id);

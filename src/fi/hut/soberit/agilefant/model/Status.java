d27828e75c8143fee91f553487b5dcc19d42aa7a
ppekkane <ppekkane@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-05-22 12:19:46 +0000

    comment fix
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@2056 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- src/fi/hut/soberit/agilefant/model/Status.java
+++ src/fi/hut/soberit/agilefant/model/Status.java
@@ -2,10 +2,7 @@ package fi.hut.soberit.agilefant.model;
 
 /**
  * A status enumeration, which represents the status of project.
- * <p>
- * Possible values are "ok", "challenged" and "critical".
  * 
- * @see fi.hut.soberit.agilefant.model.Task
  */
 public enum Status {
     GREEN, YELLOW, RED, GREY, BLACK;

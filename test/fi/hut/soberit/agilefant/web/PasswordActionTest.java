a07441caed5546c5554ccd2dc9d697b838660ba5
jtjavana <jtjavana@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-05-22 11:57:09 +0000

    Fixed build breakage (Bamboo uses JUnit 3)
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@2052 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- test/fi/hut/soberit/agilefant/web/PasswordActionTest.java
+++ test/fi/hut/soberit/agilefant/web/PasswordActionTest.java
@@ -1,8 +1,10 @@
 package fi.hut.soberit.agilefant.web;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
-import static org.easymock.EasyMock.*;
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import junit.framework.TestCase;
 
 import com.opensymphony.xwork.Action;
 
@@ -10,10 +12,25 @@ import fi.hut.soberit.agilefant.business.PasswordBusiness;
 import fi.hut.soberit.agilefant.business.UserBusiness;
 import fi.hut.soberit.agilefant.model.User;
 
-public class PasswordActionTest {
+public class PasswordActionTest extends TestCase {
     // For simplicity this class does not extend SpringTestCase
 
     private final static int TESTUSER_ID = 100;
+
+    private UserBusiness userBusiness;
+
+    private PasswordBusiness passwordBusiness;
+
+    private PasswordAction action;
+
+    @Override
+    public void setUp() {
+        userBusiness = createMock(UserBusiness.class);
+        passwordBusiness = createMock(PasswordBusiness.class);
+        action = new PasswordAction();
+        action.setUserBusiness(userBusiness);
+        action.setPasswordBusiness(passwordBusiness);
+    }
 
     private User createTestUser() {
         User user = new User();
@@ -23,17 +40,11 @@ public class PasswordActionTest {
         return user;
     }
 
-    @Test
     public void testGenerate() {
-        UserBusiness userBusiness = createMock(UserBusiness.class);
-        PasswordBusiness passwordBusiness = createMock(PasswordBusiness.class);
         passwordBusiness.generateAndMailPassword(TESTUSER_ID);
         expect(userBusiness.getUser("keimolantio")).andReturn(createTestUser());
         replay(passwordBusiness, userBusiness);
 
-        PasswordAction action = new PasswordAction();
-        action.setUserBusiness(userBusiness);
-        action.setPasswordBusiness(passwordBusiness);
         action.setName("keimolantio");
         action.setEmail("keimo.lantio@somewhere.com");
 
@@ -41,34 +52,26 @@ public class PasswordActionTest {
         verify(passwordBusiness, userBusiness);
     }
 
-    @Test
     public void testGenerateWithUnknownName() {
-        UserBusiness userBusiness = createMock(UserBusiness.class);
         expect(userBusiness.getUser("leimokantio")).andReturn(null);
-        replay(userBusiness);
+        replay(passwordBusiness, userBusiness);
 
-        PasswordAction action = new PasswordAction();
-        action.setUserBusiness(userBusiness);
         action.setName("leimokantio");
         action.setEmail("keimo.lantio@somewhere.com");
 
         assertEquals(Action.ERROR, action.generate());
-        verify(userBusiness);
+        verify(passwordBusiness, userBusiness);
     }
 
-    @Test
     public void testGenerateWithWrongEmail() {
-        UserBusiness userBusiness = createMock(UserBusiness.class);
         expect(userBusiness.getUser("keimolantio")).andReturn(createTestUser());
-        replay(userBusiness);
+        replay(passwordBusiness, userBusiness);
 
-        PasswordAction action = new PasswordAction();
-        action.setUserBusiness(userBusiness);
         action.setName("keimolantio");
         action.setEmail("leimo.kantio@somewhere.com");
 
         assertEquals(Action.ERROR, action.generate());
-        verify(userBusiness);
+        verify(passwordBusiness, userBusiness);
     }
 
 }

bed85846760b7fd79f92cdd50f47ab714596b3d9
jtjavana <jtjavana@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-05-25 13:46:08 +0000

    Added ProjectTypeBusiness and its implementation
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@2074 29784df9-99d5-7e28-3e6c-ac1830307ffc

---
+++ src/fi/hut/soberit/agilefant/business/ProjectTypeBusiness.java
@@ -0,0 +1,11 @@
+package fi.hut.soberit.agilefant.business;
+
+import java.util.Collection;
+
+import fi.hut.soberit.agilefant.model.ProjectType;
+
+public interface ProjectTypeBusiness {
+
+    public Collection<ProjectType> getAll();
+
+}

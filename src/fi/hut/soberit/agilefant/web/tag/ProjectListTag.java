b5071e7523371e2c619c40746a710883d1227e86
jtjavana <jtjavana@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-05-26 08:03:24 +0000

    ProjectDAO -> ProjectBusiness
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@2106 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- src/fi/hut/soberit/agilefant/web/tag/ProjectListTag.java
+++ src/fi/hut/soberit/agilefant/web/tag/ProjectListTag.java
@@ -3,18 +3,19 @@ package fi.hut.soberit.agilefant.web.tag;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.Tag;
 
-import fi.hut.soberit.agilefant.db.ProjectDAO;
+import fi.hut.soberit.agilefant.business.ProjectBusiness;
 
 public class ProjectListTag extends SpringTagSupport {
     private static final long serialVersionUID = -7498764572144094830L;
 
-    private ProjectDAO projectDAO;
+    private ProjectBusiness projectBusiness;
 
     @Override
     public int doStartTag() throws JspException {
-        projectDAO = requireBean("projectDAO");
+        projectBusiness = requireBean("projectBusiness");
         super.getPageContext().setAttribute(super.getId(),
-                projectDAO.getAll());
+                projectBusiness.getAll());
         return Tag.EVAL_BODY_INCLUDE;
     }
+
 }

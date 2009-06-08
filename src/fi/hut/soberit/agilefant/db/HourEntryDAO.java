2131755a74d5b3cddda9a52e465367c823aef632
praty <praty@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-06-08 10:34:44 +0000

    + Remove unused imports.
    git-svn-id: http://agilefant.googlecode.com/svn/branches/Monza_restructure@2262 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- src/fi/hut/soberit/agilefant/db/HourEntryDAO.java
+++ src/fi/hut/soberit/agilefant/db/HourEntryDAO.java
@@ -1,9 +1,5 @@
 package fi.hut.soberit.agilefant.db;
 
-import java.util.List;
-
-import fi.hut.soberit.agilefant.model.Backlog;
-import fi.hut.soberit.agilefant.model.BacklogHourEntry;
 import fi.hut.soberit.agilefant.model.HourEntry;
 
 public interface HourEntryDAO extends GenericDAO<HourEntry> {

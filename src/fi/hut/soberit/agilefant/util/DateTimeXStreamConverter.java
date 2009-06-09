e854bca17323e48d7a436f7310fbbf3a19deb7f6
jtjavana <jtjavana@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-06-09 08:03:36 +0000

    Added a simple XStream DateTime converter
    git-svn-id: http://agilefant.googlecode.com/svn/branches/Monza_restructure@2295 29784df9-99d5-7e28-3e6c-ac1830307ffc

---
+++ src/fi/hut/soberit/agilefant/util/DateTimeXStreamConverter.java
@@ -0,0 +1,31 @@
+package fi.hut.soberit.agilefant.util;
+
+import org.joda.time.DateTime;
+
+import com.thoughtworks.xstream.converters.ConversionException;
+import com.thoughtworks.xstream.converters.SingleValueConverter;
+
+public class DateTimeXStreamConverter implements SingleValueConverter {
+
+    @SuppressWarnings("unchecked")
+    public boolean canConvert(Class clazz) {
+        return clazz.isInstance(DateTime.class);
+    }
+
+    public Object fromString(String value) {
+        if (value == null || value.isEmpty()) {
+            return null;
+        }
+        long millis = Long.parseLong(value);
+        return new DateTime(millis);
+    }
+
+    public String toString(Object object) {
+        if (object instanceof DateTime) {
+            throw new ConversionException("Not a DateTime!");
+        }
+        DateTime dateTime = (DateTime) object;
+        return Long.toString(dateTime.getMillis());
+    }
+
+}

c0afb69129cd118c3d9d778055b8e12da7603852
jtjavana <jtjavana@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-06-09 09:26:34 +0000

    Added hashCode/equals/toString to Pair
    git-svn-id: http://agilefant.googlecode.com/svn/branches/Monza_restructure@2299 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- src/fi/hut/soberit/agilefant/util/Pair.java
+++ src/fi/hut/soberit/agilefant/util/Pair.java
@@ -19,7 +19,43 @@ public class Pair<First, Second> {
         return second;
     }
 
-    public static <First, Second> Pair<First, Second> create(First first, Second second) {
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((first == null) ? 0 : first.hashCode());
+        result = prime * result + ((second == null) ? 0 : second.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (!(obj instanceof Pair)) return false;
+        Pair<?, ?> other = (Pair<?, ?>) obj;
+        if (first == null) {
+            if (other.first != null) return false;
+        } else if (!first.equals(other.first)) return false;
+        if (second == null) {
+            if (other.second != null) return false;
+        } else if (!second.equals(other.second)) return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("{[");
+        builder.append(first.toString());
+        builder.append("], [");
+        builder.append(second.toString());
+        builder.append("]}");
+        return builder.toString();
+    }
+
+    public static <First, Second> Pair<First, Second> create(First first,
+            Second second) {
         return new Pair<First, Second>(first, second);
     }
 

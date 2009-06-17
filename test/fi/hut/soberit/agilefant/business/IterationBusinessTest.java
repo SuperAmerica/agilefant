b01cc285a71cfe0561c00fc7e8fed8d5b8c89abc
ppekkane <ppekkane@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-06-17 10:33:31 +0000

    + int to long cast in iteration business test
    git-svn-id: http://agilefant.googlecode.com/svn/branches/Monza_restructure@2421 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- test/fi/hut/soberit/agilefant/business/IterationBusinessTest.java
+++ test/fi/hut/soberit/agilefant/business/IterationBusinessTest.java
@@ -186,8 +186,8 @@ public class IterationBusinessTest {
         
         IterationMetrics actualMetrics = iterationBusiness.getIterationMetrics(iteration); 
         
-        assertEquals(0, actualMetrics.getEffortLeft().getMinorUnits().longValue());
-        assertEquals(0, actualMetrics.getOriginalEstimate().getMinorUnits().longValue());
+        assertEquals(0L, actualMetrics.getEffortLeft().getMinorUnits().longValue());
+        assertEquals(0L, actualMetrics.getOriginalEstimate().getMinorUnits().longValue());
         
         verify(iterationHistoryEntryBusiness);
     }

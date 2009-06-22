b99474a5695d1537f844e0642b213446342bf5e3
praty <praty@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-06-22 07:51:55 +0000

    + Add back setting genericDAO.
    git-svn-id: http://agilefant.googlecode.com/svn/branches/Monza_restructure@2450 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- src/fi/hut/soberit/agilefant/business/impl/ProjectTypeBusinessImpl.java
+++ src/fi/hut/soberit/agilefant/business/impl/ProjectTypeBusinessImpl.java
@@ -1,14 +1,20 @@
 package fi.hut.soberit.agilefant.business.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import fi.hut.soberit.agilefant.business.ProjectTypeBusiness;
+import fi.hut.soberit.agilefant.db.ProjectTypeDAO;
 import fi.hut.soberit.agilefant.model.ProjectType;
 
 @Service("projectTypeBusiness")
 @Transactional
-public class ProjectTypeBusinessImpl extends GenericBusinessImpl<ProjectType> implements
-        ProjectTypeBusiness {
-
+public class ProjectTypeBusinessImpl extends GenericBusinessImpl<ProjectType>
+        implements ProjectTypeBusiness {
+    
+    @Autowired
+    public void setProjectTypeDAO(ProjectTypeDAO projectTypeDAO) {
+        this.genericDAO = projectTypeDAO;
+    }
 }

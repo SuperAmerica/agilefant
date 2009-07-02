3db3cc12108842edcb408a2c3302ec52f60740ef
ppekkane <ppekkane@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-07-02 11:04:02 +0000

    + new hibernate version
    git-svn-id: http://agilefant.googlecode.com/svn/branches/Monza_restructure@2535 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- src/fi/hut/soberit/agilefant/model/Holiday.java
+++ src/fi/hut/soberit/agilefant/model/Holiday.java
@@ -1,19 +1,20 @@
 package fi.hut.soberit.agilefant.model;
 
-import java.sql.Date;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+import org.hibernate.annotations.Type;
+import org.joda.time.Interval;
+
 @Entity
 public class Holiday {
     private int id;
     private User user;
-    private Date startDate;
-    private Date endDate;
+    private Interval interval;
+
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -30,16 +31,12 @@ public class Holiday {
     public void setUser(User user) {
         this.user = user;
     }
-    public Date getStartDate() {
-        return startDate;
+    @Type(type = "org.joda.time.contrib.hibernate.PersistentInterval")
+    public Interval getInterval() {
+        return interval;
     }
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
+    public void setInterval(Interval interval) {
+        this.interval = interval;
     }
-    public Date getEndDate() {
-        return endDate;
-    }
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
+    
 }

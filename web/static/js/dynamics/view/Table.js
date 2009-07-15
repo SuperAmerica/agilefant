b4ae8a12ee0dda2a723f72d2a63c6dd386bee5ef
ppekkane <ppekkane@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-07-15 14:44:51 +0000

    git-svn-id: http://agilefant.googlecode.com/svn/trunk@2641 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- web/static/js/dynamics/view/Table.js
+++ web/static/js/dynamics/view/Table.js
@@ -1,3 +1,21 @@
-var DynamicTable = function() {
+var DynamicTable = function(controller, model) {
+	this.init(controller, model);
+	this.model.getDataSource = null;
+	if(typeof this.model.getDataSource === "function") {
+		this.dataSource = this.model.getDataSource();
+	}
+};
+DynamicTable.prototype = new DynamicView();
+
+DynamicTable.prototype.layout = function() {
+	
+};
+DynamicTable.prototype.render = function() {
+	
+};
+DynamicTable.prototype.createRow = function() {
+	
+};
+DynamicTable.prototype.setDataSource = function() {
 	
 };

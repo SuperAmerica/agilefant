05c0e5aaf68970aea834909a2c0cc49f260c52c7
rjokelai <rjokelai@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-07-16 12:34:13 +0000

    + Added classname to type to model factory
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@2661 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- web/static/js/dynamics/model/ModelFactory.js
+++ web/static/js/dynamics/model/ModelFactory.js
@@ -5,10 +5,22 @@
  * @see CommonModel
  * @constructor
  */
-ModelFactory = function() {};
+ModelFactory = function() {
+  
+};
 
 ModelFactory.instance = null;
 
+ModelFactory.classNameToType = {
+  "fi.hut.soberit.agilefant.model.Iteration": "iteration",
+  "fi.hut.soberit.agilefant.model.Product":   "product",
+  "fi.hut.soberit.agilefant.model.Project":   "project",
+  
+  "fi.hut.soberit.agilefant.model.Story":     "story",
+  "fi.hut.soberit.agilefant.model.StoryTO":   "story",
+  "fi.hut.soberit.agilefant.model.Task":      "task",
+  "fi.hut.soberit.agilefant.model.TaskTO":    "task"
+};
 
 /**
  * Get the singleton instance of the model factory.

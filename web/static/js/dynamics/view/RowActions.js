108f5215f52198aa41d1cc763402ed1543253966
ppekkane <ppekkane@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-07-21 07:59:37 +0000

    + table row actions implementation
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@2682 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- web/static/js/dynamics/view/RowActions.js
+++ web/static/js/dynamics/view/RowActions.js
@@ -1,14 +1,61 @@
-var DynamicTableRowActions = function(options, controller, model, parentView) {
-	
+var DynamicTableRowActions = function(items, controller, model, parentView) {
+  this.items = items;
+  this.controller = controller;
+  this.model = model;
+  this.parentView = parentView;
+  this.menuOpen = false;
+  this.initialize();
 };
-DynamicTableRowActions.prototype = new DynamicView();
+
+DynamicTableRowActions.prototype.initialize = function() {
+  var me = this;
+  this.container = $('<div />').width("68px").appendTo(
+      this.parentView.getElement());
+  this.button = $('<div class="actionColumn"><div class="edit"><div class="gear" style="float: left;"/><div style="float: right">Edit</div></div></div>');
+  this.button.appendTo(this.container);
+
+  this.toggleMenuListener = function(event) {
+    if (me.menuOpen) {
+      me.close();
+    } else {
+      me.open();
+    }
+    event.stopPropagation();
+    return false;
+  };
+  this.button.click(this.toggleMenuListener);
+};
 
 DynamicTableRowActions.prototype.open = function() {
-  
+  var me = this;
+  $(window).click(this.toggleMenuListener);
+  this.menuOpen = true;
+  this.menu = $('<ul/>').appendTo(document.body).addClass("actionCell");
+  var off = this.parentView.getElement().offset();
+  var menuCss = {
+    "position" : "absolute",
+    "overflow" : "visible",
+    "z-index" : "100",
+    "white-space" : "nowrap",
+    "top" : off.top + 18,
+    "left" : off.left - 32
+  };
+  this.menu.css(menuCss);
+  $.each(this.items, function(index, item) {
+    var it = $('<li />').text(item.text).appendTo(me.menu);
+    it.click(function() {
+      me._click(index);
+      return false;
+    });
+  });
 };
+
 DynamicTableRowActions.prototype.close = function() {
-  
+  $(window).unbind('click', this.toggleMenuListener);
+  this.menu.remove();
+  this.menuOpen = false;
 };
+
 DynamicTableRowActions.prototype._click = function(index) {
-  
+  this.items[index].callback.call(this.controller, this.model, this.parentView);
 };
--- web/static/js/tests/dynamics/view/RowAction.test.js
+++ web/static/js/tests/dynamics/view/RowAction.test.js
@@ -3,10 +3,46 @@ $(document).ready(function() {
 		setup: function() {
 			this.parent = $('<div />').appendTo(document.body);
 			this.mockControl = new MockControl();
-			
+			this.parentView = this.mockControl.createMock(DynamicView);
+			this.testController = function() {
+			};
+			this.testController.prototype.func1 = function(model, view) {
+			};
+			this.testController.prototype.func2 = function(model, view) {
+			};
+			this.controller = this.mockControl.createMock(this.testController);
 		}, teardown: function() {
-		
+		  this.parent.remove();
 			this.mockControl.verify();
 		}
+	});
+	
+	test("..", function() {
+    this.parentView.expects().getElement().andReturn(this.parent);
+    
+    var items = [{text: "Item 1", callback: this.controller.func1},
+                 {text: "Item 2", callback: this.controller.func2}
+                 ];
+    var testable = new DynamicTableRowActions(items, this.controller, null, this.parentView);
+    var menuButton = this.parent.children("div").children("div.actionColumn");
+   
+    equals($('.actionCell').length, 0, "Menu not open");
+    //expect when opening the menu
+    this.parentView.expects().getElement().andReturn(this.parent);
+    menuButton.click();
+    equals($('.actionCell').length, 1, "Menu open");
+
+    //click first menu item
+    this.controller.expects().func1(null, this.parentView);
+    $('ul.actionCell').children("li:eq(0)").click();
+    //and the second
+    this.controller.expects().func2(null, this.parentView);
+    $('ul.actionCell').children("li:eq(1)").click();
+    //check that the menu is still open
+    ok($('.actionCell').is(":visible"), "Menu visible");
+    //close the menu
+    $(window).click();
+    equals($('.actionCell').length, 0, "Menu not open");
+    
 	});
 });

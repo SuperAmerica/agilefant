927545f04459a44da613b1baac92839147e0e138
rjokelai <rjokelai@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-07-23 10:01:38 +0000

    + Story saving should now work
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@2723 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- web/static/js/dynamics/model/StoryModel.js
+++ web/static/js/dynamics/model/StoryModel.js
@@ -38,6 +38,28 @@ StoryModel.prototype._setData = function(newData) {
   }
 };
 
+/**
+ * Internal function to send the data to server.
+ */
+StoryModel.prototype._saveData = function(id, changedData) {
+  var url = "ajax/storeStory.action";
+  var data = changedData;
+  jQuery.extend(data, {storyId: id});
+  
+  jQuery.ajax({
+    type: "POST",
+    async: true,
+    cache: false,
+    data: data,
+    dataType: "json",
+    success: function(data, status) {
+      alert("Story saved");
+    },
+    error: function(request, status, error) {
+      alert("Error saving story");
+    }
+  });
+};
 
 // Getters and setters in property alphabetical order
 StoryModel.prototype.getBacklog = function() {
@@ -51,16 +73,33 @@ StoryModel.prototype.setBacklog = function(backlog) {
 StoryModel.prototype.getDescription = function() {
   return this.currentData.description;
 };
+StoryModel.prototype.setDescription = function(description) {
+  this.currentData.description = description;
+  this._commitIfNotInTransaction();
+};
 
 StoryModel.prototype.getName = function() {
   return this.currentData.name;
+};
+StoryModel.prototype.setName = function(name) {
+  this.currentData.name = name;
+  this._commitIfNotInTransaction();
 };
 
 StoryModel.prototype.getState = function() {
   return this.currentData.state;
 };
+StoryModel.prototype.setState = function(state) {
+  this.currentData.state = state;
+  this._commitIfNotInTransaction();
+};
 
 StoryModel.prototype.getStoryPoints = function() {
   return this.currentData.storyPoints;
 };
+StoryModel.prototype.getStoryPoints = function(storyPoints) {
+  this.currentData.storyPoints = storyPoints;
+  this._commitIfNotInTransaction();
+};
+
 

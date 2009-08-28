24043aff925bb0d5b219bdbfd39a962d00a12a69
rjokelai <rjokelai@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-08-28 06:27:22 +0000

    + Changed the way MessageDisplay works
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@2945 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- web/static/js/dynamics/view/MessageDisplay.js
+++ web/static/js/dynamics/view/MessageDisplay.js
@@ -2,12 +2,22 @@
 var MessageDisplay = {};
 
 MessageDisplay.cssClasses = {
+  messageBox:     "dynamicsMessageContainer",
+  
   genericMessage: "genericMessage",
   okMessage:      "okMessage",
   errorMessage:   "errorMessage",
   
   messageArea:    "messageArea",
   closeButton:    "closeButton"
+};
+
+MessageDisplay.messageList = null;
+
+MessageDisplay.initMessageList = function() {
+  if (!MessageDisplay.messageList) {
+    MessageDisplay.messageList = $('<ul/>').addClass(MessageDisplay.cssClasses.messageBox).appendTo(document.body);
+  }
 };
 
 /**
@@ -17,39 +27,66 @@ MessageDisplay.cssClasses = {
 MessageDisplay.GenericMessage = function() {};
 MessageDisplay.GenericMessage.prototype = new ViewPart(); 
 
-
+/**
+ * Initialize a new generic message box.
+ */
 MessageDisplay.GenericMessage.prototype.init = function() {
   var me = this;
-  this.element = $('<div/>').appendTo(document.body);
+  MessageDisplay.initMessageList();
+  
+  // Create the element
+  this.element = $('<li/>').appendTo(MessageDisplay.messageList);
   this.element.addClass(MessageDisplay.cssClasses.genericMessage);
+  this.messageArea = this.element;
+  
+  this.render();
+  
+  if (this.options.displayTime) {
+    this.fadeOutTimer(this.options.displayTime);
+  }
+};
+
+/**
+ * Renders the inner elements of the message. 
+ */
+MessageDisplay.GenericMessage.prototype.render = function() {
+  var me = this;
   if (this.options.closeButton) {
-    $('<div/>').addClass(MessageDisplay.cssClasses.messageArea).text(this.message).appendTo(this.element);
+    this.messageArea = $('<div/>').addClass(MessageDisplay.cssClasses.messageArea).appendTo(this.element);
     var close = $('<div/>').addClass(MessageDisplay.cssClasses.closeButton).text('X').appendTo(this.element);
     
     close.click(function() {
       me.fadeOut();
     });
   }
-  else {
-    this.element.text(this.message);
-  }
-  if (this.options.displayTime) {
-    this.fadeOutTimer();
-  }
-};
-MessageDisplay.GenericMessage.prototype.fadeOut = function() {
-  var me = this;
-  var remove = function() {
-    me.element.remove();
-  };
-  this.element.fadeOut(this.options.fadeOutTime, remove);
+  this.addContent();
 };
 
-MessageDisplay.GenericMessage.prototype.fadeOutTimer = function() {
+MessageDisplay.GenericMessage.prototype.addContent = function() {
+  this.messageArea.text(this.message);
+};
+
+/**
+ * Tell the message to fade out and destroy.
+ */
+MessageDisplay.GenericMessage.prototype.fadeOut = function() {
+  var me = this;
+  this.element.hide('blind',{},this.options.fadeOutTime, function() { me.destroy(); });
+//  this.element.fadeOut(this.options.fadeOutTime, function() { me.destroy(); });
+};
+
+MessageDisplay.GenericMessage.prototype.destroy = function() {
+  this.element.remove();
+};
+
+/**
+ * Tells the message to fade out after <code>displayTime</code> milliseconds.
+ */
+MessageDisplay.GenericMessage.prototype.fadeOutTimer = function(displayTime) {
   var me = this;
   setTimeout(function() {
     me.fadeOut();
-  }, this.options.displayTime);
+  }, displayTime);
 };
 
 /*
@@ -57,12 +94,12 @@ MessageDisplay.GenericMessage.prototype.fadeOutTimer = function() {
  */
 MessageDisplay.ErrorMessage = function(message, xhr, opts) {
   this.options = {
-      fadeOutTime: 200,
+      fadeOutTime: 1000,
       closeButton: true
   };
   this.message = message;
   if(xhr) {
-  this.jsonData = jQuery.httpData(xhr, "json", null);
+    this.jsonData = jQuery.httpData(xhr, "json", null);
     jQuery.extend(this.options, opts);
     this.detailedMessage = this.jsonData.errorMessage;
     this.trace = this.jsonData.trace;
@@ -72,6 +109,22 @@ MessageDisplay.ErrorMessage = function(message, xhr, opts) {
 };
 
 MessageDisplay.ErrorMessage.prototype = new MessageDisplay.GenericMessage();
+
+MessageDisplay.ErrorMessage.prototype.addContent = function() {
+  var message = "<span>" + this.message + "</span>";
+  
+  if (this.jsonData) {
+    var list = "<dl>";
+    
+    list += "<dt>Message</dt><dd>" + this.detailedMessage + "</dd>";
+    list += "<dt>Trace</dt><dd>" + this.trace + "</dd>";
+    
+    list += "</dl>";
+    
+    message += list;
+  }
+  this.messageArea.html(message);
+};
 
 /*
  * OK MESSAGE

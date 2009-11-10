5399e23f3cedaccc91ceb738c152713d6356c642
rjokelai <rjokelai@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-11-10 13:57:55 +0000

    + Added edit menu to story tree + Stories can now be split in story tree
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@3348 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- web/static/js/dynamics/controller/StoryTreeController.js
+++ web/static/js/dynamics/controller/StoryTreeController.js
@@ -20,26 +20,81 @@ StoryTreeController.prototype.refresh = function() {
   $(this.element).load("ajax/getProjectStoryTree.action", {projectId: this.id});
 };
 
+StoryTreeController.prototype._getStoryForId = function(id, callback) {
+  var model = ModelFactory.getOrRetrieveObject(
+    ModelFactory.types.story,
+    id,
+    function(type, id, object) {
+      callback(object);
+    },
+    function(xhr, status, error) {
+      MessageDisplay.Error('Story cannot be loaded', xhr);
+    }
+  );
+};
+
 /**
  * Initializes the tree.
  * 
  * Will send an ajax request.
  */
 StoryTreeController.prototype._initializeTree = function() {
+  var me = this;
+  var elem = $(this.element);
   
-  $(this.element).find('li > span').live('click', function() {
-    var id = $(this).parent().attr('storyid');
-    
-    var model = ModelFactory.getOrRetrieveObject(
-        ModelFactory.types.story,
-        id,
-        function(type, id, object) {
-          var dialog = new StoryInfoDialog(object);
-        },
-        function(xhr, status, error) {
-          MessageDisplay.Error('Story cannot be loaded', xhr);
-        });
+  /* Show edit button */
+  elem.find('li > span').live('mouseover', function() {
+    if (!$(this).children('.editButton').get(0)) {
+      $('.editButton').remove();
+      var button = $('<div />').addClass('editButton').html('Edit &#8711;').appendTo($(this));
+    }
   });
+  
+  
+  /* Edit menu creation*/
+  elem.find('.editButton').live('click', function() {
+    var off = $(this).offset();
+    var menu = $('<ul/>').addClass('editButtonMenu').appendTo($(this));
+    menu.css({
+      "top" : off.top + 18,
+      "left" : off.left - 32
+    });
+    var editButton = $('<li/>').text('Edit').appendTo(menu);
+    var splitButton = $('<li/>').text('Split').appendTo(menu);
+    var deleteButton = $('<li/>').text('Delete').appendTo(menu);
+    
+    /*
+     * Click handler for edit button
+     */
+    editButton.click(function() {
+      var id = $(this).parents('li:eq(0)').attr('storyid');
+      me._getStoryForId(id, function(object) {
+        var dialog = new StoryInfoDialog(object);
+      });
+      menu.remove();
+    });
+    
+    /*
+     * Click handler for split button
+     */
+    splitButton.click(function() {
+      var id = $(this).parents('li:eq(0)').attr('storyid');
+      me._getStoryForId(id, function(object) {
+        var dialog = new StorySplitDialog(object);
+      });
+      menu.remove();
+    });
+    
+    /*
+     * Click handler for delete button
+     */
+    deleteButton.click(function() {
+      alert("Not implemented");
+      menu.remove();
+    });
+  });
+
+  
 };
 
 

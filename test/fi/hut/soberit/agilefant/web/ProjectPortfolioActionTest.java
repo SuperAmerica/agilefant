511b63f40668507bbf44d430d6f2a5ca98771bb3
jtjavana <jtjavana@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2009-11-18 12:07:24 +0000

    New: Added tests for ProjectPortfolioAction
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@3400 29784df9-99d5-7e28-3e6c-ac1830307ffc

---
+++ test/fi/hut/soberit/agilefant/web/ProjectPortfolioActionTest.java
@@ -0,0 +1,74 @@
+package fi.hut.soberit.agilefant.web;
+
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.opensymphony.xwork2.Action;
+
+import fi.hut.soberit.agilefant.business.PortfolioBusiness;
+import fi.hut.soberit.agilefant.test.Mock;
+import fi.hut.soberit.agilefant.test.MockContextLoader;
+import fi.hut.soberit.agilefant.test.SpringAssertions;
+import fi.hut.soberit.agilefant.test.TestedBean;
+import fi.hut.soberit.agilefant.transfer.PortfolioTO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader = MockContextLoader.class)
+public class ProjectPortfolioActionTest {
+
+    @TestedBean
+    private ProjectPortfolioAction projectPortfolioAction;
+
+    @Mock
+    private PortfolioBusiness portfolioBusiness;
+
+    protected void replayAll() {
+        replay(portfolioBusiness);
+    }
+
+    protected void verifyAll() {
+        verify(portfolioBusiness);
+    }
+
+    // ProjectPortfolioAction should
+    // + have Scope annotation with "prototype" value
+    @Test
+    public void testSpringScope() {
+        SpringAssertions.assertScopeAnnotation("prototype",
+                ProjectPortfolioAction.class);
+    }
+
+    // retrieve() should
+    // + return Action.SUCCESS
+    @Test
+    @DirtiesContext
+    public void testRetrieve() {
+        assertEquals(Action.SUCCESS, projectPortfolioAction.retrieve());
+    }
+
+    // portfolioData() should
+    // + retrieve a PortfolioTO from PortfolioBusiness
+    // + set PortfolioTO field
+    // + return Action.SUCCESS
+    @Test
+    @DirtiesContext
+    public void testPortfolioData() {
+        PortfolioTO portfolioTO = new PortfolioTO();
+        expect(portfolioBusiness.getPortfolioData()).andReturn(portfolioTO);
+        replayAll();
+
+        assertEquals(Action.SUCCESS, projectPortfolioAction.portfolioData());
+        assertSame(portfolioTO, projectPortfolioAction.getPortfolioData());
+        verifyAll();
+    }
+
+}

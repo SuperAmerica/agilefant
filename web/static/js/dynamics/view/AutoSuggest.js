5651b7367ab837cc53e44d8138ba22dd4bf20bc6
aherttua <aherttua@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2010-01-23 12:17:53 +0000

    Buttons to LabelsView
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@3569 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- web/static/js/dynamics/view/AutoSuggest.js
+++ web/static/js/dynamics/view/AutoSuggest.js
@@ -8,6 +8,8 @@ AutoSuggest.prototype = new ViewPart();
 
 AutoSuggest.prototype.initialize = function() {
   this.element = $("<div></div>");
+  this.okButton = $('<img src="static/img/ok.png" alt="Ok" />').appendTo(this.element);
+  this.cancelButton = $('<img src="static/img/cancel.png" alt="Cancel" />').appendTo(this.element);
   $('<input type="text" />').appendTo(this.element).autoSuggest(this.options.source, this.options);
   this.selectionsElement = this.element.find(".as-selections");
   this.valuesElement = this.selectionsElement.find(".as-values");
@@ -51,6 +53,14 @@ AutoSuggest.prototype.initialize = function() {
     }
   });
   this.element.appendTo(this.parentView.getElement());
+  
+  this.okButton.click(function() {
+    me.success();
+  });
+	  
+  this.cancelButton.click(function() {
+    me.cancel();
+  });
 };
 
 AutoSuggest.prototype.success = function() {

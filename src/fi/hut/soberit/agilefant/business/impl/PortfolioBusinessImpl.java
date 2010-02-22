1ca38f6baf4bbc225fe1396b3ba070f6485503e5
jtjavana <jtjavana@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2010-02-22 18:48:37 +0000

    Fix: Fill projectTO.assignees in PortfolioTO
    + Don't use assignments but assignees instead
    + Fixes portfolio assignees column (+ editing)
    
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@3709 29784df9-99d5-7e28-3e6c-ac1830307ffc

--- src/fi/hut/soberit/agilefant/business/impl/PortfolioBusinessImpl.java
+++ src/fi/hut/soberit/agilefant/business/impl/PortfolioBusinessImpl.java
@@ -1,5 +1,10 @@
 package fi.hut.soberit.agilefant.business.impl;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.joda.time.Days;
 import org.joda.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +14,11 @@ import org.springframework.transaction.annotation.Transactional;
 import fi.hut.soberit.agilefant.business.PortfolioBusiness;
 import fi.hut.soberit.agilefant.business.SettingBusiness;
 import fi.hut.soberit.agilefant.db.ProjectDAO;
+import fi.hut.soberit.agilefant.model.Assignment;
+import fi.hut.soberit.agilefant.model.Project;
+import fi.hut.soberit.agilefant.model.User;
 import fi.hut.soberit.agilefant.transfer.PortfolioTO;
+import fi.hut.soberit.agilefant.transfer.ProjectTO;
 
 @Service("portfolioBusiness")
 @Transactional
@@ -25,12 +34,33 @@ public class PortfolioBusinessImpl implements PortfolioBusiness {
     public PortfolioTO getPortfolioData() {
         PortfolioTO portfolioData = new PortfolioTO();
         LocalDate startDate = new LocalDate();
-        LocalDate endDate = startDate.plus(settingBusiness.getPortfolioTimeSpan());
-        portfolioData.setRankedProjects(projectDAO.getRankedProjects(startDate,
-                endDate));
-        portfolioData.setUnrankedProjects(projectDAO.getUnrankedProjects(
-                startDate, endDate));
-        portfolioData.setTimeSpanInDays(Days.daysBetween(startDate, endDate).getDays());
+        LocalDate endDate = startDate.plus(settingBusiness
+                .getPortfolioTimeSpan());
+        List<Project> rankedProjects = new ArrayList<Project>();
+        for (Project project : projectDAO.getRankedProjects(startDate, endDate)) {
+            ProjectTO to = new ProjectTO(project);
+            Set<User> assignees = new HashSet<User>();
+            for (Assignment assignment : to.getAssignments()) {
+                assignees.add(assignment.getUser());
+            }
+            to.setAssignees(assignees);
+            rankedProjects.add(to);
+        }
+        portfolioData.setRankedProjects(rankedProjects);
+        Set<Project> unrankedProjects = new HashSet<Project>();
+        for (Project project : projectDAO.getUnrankedProjects(startDate,
+                endDate)) {
+            ProjectTO to = new ProjectTO(project);
+            Set<User> assignees = new HashSet<User>();
+            for (Assignment assignment : to.getAssignments()) {
+                assignees.add(assignment.getUser());
+            }
+            to.setAssignees(assignees);
+            unrankedProjects.add(to);
+        }
+        portfolioData.setUnrankedProjects(unrankedProjects);
+        portfolioData.setTimeSpanInDays(Days.daysBetween(startDate, endDate)
+                .getDays());
         return portfolioData;
     }
 
--- test/fi/hut/soberit/agilefant/business/PortfolioBusinessTest.java
+++ test/fi/hut/soberit/agilefant/business/PortfolioBusinessTest.java
@@ -3,6 +3,7 @@ package fi.hut.soberit.agilefant.business;
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertSame;
 
@@ -20,12 +21,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import fi.hut.soberit.agilefant.business.impl.PortfolioBusinessImpl;
 import fi.hut.soberit.agilefant.db.ProjectDAO;
+import fi.hut.soberit.agilefant.model.Assignment;
 import fi.hut.soberit.agilefant.model.Project;
+import fi.hut.soberit.agilefant.model.User;
 import fi.hut.soberit.agilefant.test.Mock;
 import fi.hut.soberit.agilefant.test.MockContextLoader;
 import fi.hut.soberit.agilefant.test.SpringAssertions;
 import fi.hut.soberit.agilefant.test.TestedBean;
 import fi.hut.soberit.agilefant.transfer.PortfolioTO;
+import fi.hut.soberit.agilefant.transfer.ProjectTO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(loader = MockContextLoader.class)
@@ -59,15 +63,28 @@ public class PortfolioBusinessTest {
     // + retrieve ranked projects from ProjectDAO
     // + retrieve unranked projects from ProjectDAO
     // + return a non-null PortfolioTO that contains
-    // a list of ranked projects and a list of unranked projects
+    // a list of ranked projects as ProjectTOs and a list of unranked projects
+    // as ProjectTOs
     @Test
     @DirtiesContext
     public void testGetPortfolioData() {
         LocalDate today = new LocalDate();
         Period timeSpan = Months.months(6).toPeriod();
         LocalDate endDate = today.plus(timeSpan);
+
+        Project rankedProject = new Project();
+        User user = new User();
+        Assignment assignment = new Assignment();
+        assignment.setUser(user);
+
         List<Project> rankedProjects = new ArrayList<Project>();
+        rankedProject.getAssignments().add(assignment);
+        rankedProjects.add(rankedProject);
+
         List<Project> unrankedProjects = new ArrayList<Project>();
+        Project unrankedProject = new Project();
+        unrankedProject.getAssignments().add(assignment);
+        unrankedProjects.add(unrankedProject);
 
         expect(settingBusiness.getPortfolioTimeSpan()).andReturn(timeSpan);
         expect(projectDAO.getRankedProjects(today, endDate)).andReturn(
@@ -78,8 +95,19 @@ public class PortfolioBusinessTest {
 
         PortfolioTO result = portfolioBusiness.getPortfolioData();
         assertNotNull(result);
-        assertSame(rankedProjects, result.getRankedProjects());
-        assertSame(unrankedProjects, result.getUnrankedProjects());
+        assertEquals(1, result.getRankedProjects().size());
+        ProjectTO rankedTo = (ProjectTO) result.getRankedProjects().get(0);
+
+        assertEquals(1, rankedTo.getAssignees().size());
+        assertSame(user, rankedTo.getAssignees().iterator().next());
+
+        assertEquals(1, result.getUnrankedProjects().size());
+        ProjectTO unrankedTo = (ProjectTO) result.getUnrankedProjects()
+                .iterator().next();
+
+        assertEquals(1, unrankedTo.getAssignees().size());
+        assertSame(user, unrankedTo.getAssignees().iterator().next());
+
         verifyAll();
     }
 

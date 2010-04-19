c9639b0abfe0eb1d1cff3b7934cbcaf2a4e41e05
rjokelai <rjokelai@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2010-04-19 12:17:15 +0000

    + Added fix-tree-ranks sql script
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@3800 29784df9-99d5-7e28-3e6c-ac1830307ffc

---
+++ doc/sql/updates/fix-tree-ranks.sql
@@ -0,0 +1,65 @@
+﻿
+DROP PROCEDURE IF EXISTS UpdateTreeRanksForChildren;
+DROP PROCEDURE IF EXISTS UpdateStoryRanksByParents;
+
+DELIMITER //
+
+CREATE PROCEDURE UpdateTreeRanksForChildren(IN par_id INT)
+BEGIN
+  DECLARE child_loop_done BOOL DEFAULT FALSE;
+  DECLARE child_id INT;
+  DECLARE current_rank_no INT DEFAULT 0;
+  DECLARE cur_child
+    CURSOR FOR
+    SELECT DISTINCT id FROM stories WHERE parent_id=par_id ORDER BY treeRank ASC;
+    
+  DECLARE CONTINUE HANDLER FOR SQLSTATE '02000'
+    SET child_loop_done = TRUE;
+    
+  OPEN cur_child;
+  
+  childLoop: LOOP
+    FETCH cur_child INTO child_id;
+    
+    IF child_loop_done THEN
+      CLOSE cur_child;
+      LEAVE childLoop;
+    END IF;
+
+    SELECT child_id AS id, current_rank_no;
+    UPDATE stories SET treeRank=current_rank_no WHERE id=child_id;
+    SET current_rank_no = current_rank_no + 1;
+  END LOOP;
+END //
+
+CREATE PROCEDURE UpdateStoryRanksByParents()
+BEGIN
+	DECLARE story_loop_done BOOL DEFAULT FALSE;
+	DECLARE par_id INT;
+	DECLARE cur_story
+	  CURSOR FOR
+	  SELECT DISTINCT parent_id FROM stories WHERE parent_id IS NOT NULL;
+
+	DECLARE CONTINUE HANDLER FOR SQLSTATE '02000'
+	  SET story_loop_done = TRUE;
+	  
+	OPEN cur_story;
+	
+	storyLoop: LOOP
+	  FETCH cur_story INTO par_id;
+	  
+	  IF story_loop_done THEN
+	    CLOSE cur_story;
+	    LEAVE storyLoop;
+	  END IF;
+    
+	  CALL UpdateTreeRanksForChildren(par_id);
+  END LOOP;
+END //
+
+DELIMITER ;
+
+CALL UpdateStoryRanksByParents();
+
+DROP PROCEDURE IF EXISTS UpdateStoryRanksByParents;
+DROP PROCEDURE IF EXISTS UpdateTreeRanksForChildren;

68fca150bd0b5878ce2202bb0b18ae8f72805941
rjokelai <rjokelai@29784df9-99d5-7e28-3e6c-ac1830307ffc>
2010-07-09 11:40:45 +0000

    + Static content caching
    git-svn-id: http://agilefant.googlecode.com/svn/trunk@4192 29784df9-99d5-7e28-3e6c-ac1830307ffc

---
+++ src/fi/hut/soberit/agilefant/web/filters/ResponseHeaderFilter.java
@@ -0,0 +1,34 @@
+package fi.hut.soberit.agilefant.web.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+public class ResponseHeaderFilter implements Filter {
+
+    private int expirationTime; 
+    
+    public void init(FilterConfig filterConfig) throws ServletException {
+        expirationTime = Integer.valueOf(filterConfig.getInitParameter("expirationTime"));
+    }
+    
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain filterChain) throws IOException, ServletException {
+        
+        HttpServletResponse httpResp = (HttpServletResponse)response;
+        
+        httpResp.addHeader("Cache-Control", "max-age=" + expirationTime);
+        
+        filterChain.doFilter(request, response);
+    }
+
+    public void destroy() {
+    }
+
+}

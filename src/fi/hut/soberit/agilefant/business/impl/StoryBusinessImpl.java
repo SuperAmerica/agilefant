22fe99264d8a1e4d7fea3d523921d4ac8e0b46af
Aleksi Hoffman <aleksi@lekksi.com>
2012-01-23 16:43:03 +0200

    WorkItem: 3502 Fixed story history table migration and story value save

--- src/fi/hut/soberit/agilefant/business/impl/StoryBusinessImpl.java
+++ src/fi/hut/soberit/agilefant/business/impl/StoryBusinessImpl.java
@@ -193,6 +193,7 @@ public class StoryBusinessImpl extends GenericBusinessImpl<Story> implements
         persisted.setDescription(dataItem.getDescription());
         persisted.setName(dataItem.getName());
         persisted.setState(dataItem.getState());
+        persisted.setStoryValue(dataItem.getStoryValue());
         persisted.setStoryPoints(dataItem.getStoryPoints());
         persisted.setParent(dataItem.getParent());
     }

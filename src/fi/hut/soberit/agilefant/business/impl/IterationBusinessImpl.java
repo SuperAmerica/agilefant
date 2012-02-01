66ed6b0c9e84b424908f9813bfddc53ee699c849
Jordan Ell <jordan.ell7@gmail.com>
2012-01-31 19:52:30 -0800

    WorkItem: 3564 Iteration metrics now display in correct order and have effort spend metric

--- src/fi/hut/soberit/agilefant/business/impl/IterationBusinessImpl.java
+++ src/fi/hut/soberit/agilefant/business/impl/IterationBusinessImpl.java
@@ -314,6 +314,7 @@ public class IterationBusinessImpl extends GenericBusinessImpl<Iteration>
         //6. calculate percentages
         metrics.setPercentDoneTasks(calculatePercent(pairTasks.first, pairTasks.second));
         metrics.setPercentDoneStories(calculatePercent(pairStories.first, pairStories.second));
+        metrics.setPercentSpentEffort(calculatePercent(metrics.getSpentEffort().intValue(), metrics.getOriginalEstimate().intValue()));
         metrics.setDoneStoryPointsPercentage(calculatePercent(metrics.getDoneStoryPoints(), metrics.getStoryPoints()));
         metrics.setDaysLeftPercentage(calculatePercent(metrics.getDaysLeft(), metrics.getTotalDays()));
         if(metrics.getEffortLeft() != null && metrics.getOriginalEstimate() != null) {

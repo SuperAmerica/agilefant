12933ead8e6ae428894c14e2f5a6b9fc02111043
Petteri Räty <petsku@petteriraty.eu>
2012-02-02 12:09:21 +0200

    WorkItem: 3571 fix update script to adjust version

--- doc/sql/upgrader/205-206.sql
+++ doc/sql/upgrader/205-206.sql
@@ -1,2 +1,3 @@
+INSERT INTO settings (`name`, `value`, `description`) VALUES ('AgilefantDatabaseVersion', '206', 'Agilefant database version') ON DUPLICATE KEY UPDATE `value`="206";
 ALTER TABLE stories ADD COLUMN storyValue INT AFTER storyPoints;
 ALTER TABLE stories_AUD ADD COLUMN storyValue INT AFTER storyPoints;

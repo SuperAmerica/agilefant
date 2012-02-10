524844e3ef945af7eba6d08270da65ad2d9ae3c1
metermaid <rita@ritacheng.com>
2012-02-10 09:12:01 -0800

    WorkItem: 3598 Working implementation

--- web/static/js/dynamics/controller/StoryInfoBubble.js
+++ web/static/js/dynamics/controller/StoryInfoBubble.js
@@ -44,35 +44,72 @@ StoryInfoBubble.prototype.checkForMoveStory = function(model) {
   }
 };
 
-StoryInfoBubble.prototype.confirmTasksToDone = function(model) {
-  var changedData = model.getChangedData();
-  var tasks = model.getTasks();
-  if (changedData.state && changedData.state === "DONE" && tasks.length > 0) {
-    var nonDoneTasks = false;
-    for (var i = 0; i < tasks.length; i++) {
-      if (tasks[i].getState() !== "DONE") {
-        nonDoneTasks = true;
-      }
-    }
-    if (nonDoneTasks) {
-      var msg = new DynamicsConfirmationDialog(
-          "Set all tasks' states to done?",
-          "Do you want to mark all tasks as done as well?",
-          function() {
-            model.currentData.tasksToDone = true;
-            model.commit();
-          },
-          function() {
-            model.commit();
-          }
-        );
-    } else {
-      model.commit();
-    }
-  }
-  else {
-    model.commit();
-  }
+StoryInfoBubble.prototype.confirmTasksAndChildrenToDone = function(model, storyTree, isTopStory) {
+	var tasks = model.getTasks();
+	var children = model.getChildren();
+	var nonDoneChildren = false;
+	var nonDoneTasks = false;
+	if (children.length > 0) {
+		for (var i = 0; i < children.length; i++) {
+		  if (children[i].getState() !== "DONE") {
+			nonDoneChildren = true;
+		  }
+		}
+	}
+	if (tasks.length > 0) {
+		for (var i = 0; i < tasks.length; i++) {
+		  if (tasks[i].getState() !== "DONE") {
+			nonDoneTasks = true;
+		  }
+		}
+	}
+	if (nonDoneChildren || nonDoneTasks) {
+	  if (isTopStory) {
+		if (changedData.state && changedData.state === "DONE" && tasks.length > 0) {
+		  var msg = new DynamicsConfirmationDialog(
+			  "Set all tasks' and stories' states to done?",
+			  "The '" + model.getName() + "' story has undone child tasks/stories! Do you want to set them Done as well?",
+			  function() {
+				for (var i = 0; i < children.length; i++) {
+				  if (children[i].getState() !== "DONE") {
+					 children[i].setState("DONE");
+					 children[i].commit();
+					 storyTree._getStoryForId(children[i].getId(), function(object) {
+						StoryInfoBubble.prototype.confirmTasksAndChildrenToDone(object, storyTree, false);
+					});
+					storyTree.refresh();
+				  }
+				}
+				if (nonDoneTasks)
+					model.currentData.tasksToDone = true;
+				model.commit();
+				storyTree.refresh();
+			  },
+			  function() {
+				model.commit();
+			  }
+			);
+			}
+		} else {
+			for (var i = 0; i < children.length; i++) {
+				if (children[i].getState() !== "DONE") {
+					children[i].setState("DONE");
+					children[i].commit();
+					storyTree._getStoryForId(children[i].getId(), function(object) {
+						StoryInfoBubble.prototype.confirmTasksAndChildrenToDone(object, storyTree, false);
+					});
+				storyTree.refresh();
+				}
+			}
+			if (nonDoneTasks)
+				model.currentData.tasksToDone = true;
+			model.commit();
+			storyTree.refresh();
+		}
+	} else {
+	  model.commit();
+	  storyTree.refresh();
+	}
 };
 
 StoryInfoBubble.prototype.handleModelEvents = function(event) {
@@ -174,12 +211,15 @@ StoryInfoBubble.prototype.addLinks = function() {
  * Create the configuration for the dynamic table.
  */
 StoryInfoBubble.prototype._createConfig = function() {
+  var toDoneFunction = function (model) {
+	StoryInfoBubble.prototype.confirmTasksAndChildrenToDone (model, this.treeController, true);
+	}
   var config = new DynamicTableConfiguration( {
     leftWidth: '25%',
     rightWidth: '74%',
     closeRowCallback: null,
     beforeCommitFunction: StoryInfoBubble.prototype.checkForMoveStory,
-	beforeCommitFunction: StoryInfoBubble.prototype.confirmTasksToDone,
+	beforeCommitFunction: toDoneFunction,
     validators: [ ]
   });
   config.addColumnConfiguration(0, {

de09016d4f11fd6b6e3cd309349668199c83713d
Dustin <dustin_fennell@yahoo.com>
2012-02-10 13:48:16 -0800

    WorkItem: 3572 Actually got the readonly servlet to throw up some error. This is... a success?

---
+++ src/fi/hut/soberit/agilefant/readonly/ReadonlyFilter.java
@@ -0,0 +1,37 @@
+package fi.hut.soberit.agilefant.readonly;
+
+import java.io.IOException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.springframework.web.filter.GenericFilterBean;
+
+public class ReadonlyFilter extends GenericFilterBean {
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+      InitialContext ictx;
+      Context environment;
+      Boolean remoteServicesEnabled = false;
+      
+      try {
+          ictx = new InitialContext();
+          environment = (Context)ictx.lookup("java:comp/env");
+          remoteServicesEnabled = (Boolean)environment.lookup("remoteEnabled");
+      } catch (NamingException e) {
+          throw new ServletException("Remote services not enabled");
+      }
+      if (!remoteServicesEnabled) {
+          throw new ServletException("Remote services not enabled");
+      }
+      
+      chain.doFilter(request, response);
+    }
+
+}

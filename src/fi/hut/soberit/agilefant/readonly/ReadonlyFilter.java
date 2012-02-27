a34cd3c9adf3b2238f2c91d364b0a6d5b5c1d4f6
Dustin <dustin_fennell@yahoo.com>
2012-02-26 16:49:08 -0800

    WorkItem: 3858 Hibernate is giving me error messages now. And a nice new copy of build.properties for everyone, enjoy.

--- src/fi/hut/soberit/agilefant/readonly/ReadonlyFilter.java
+++ src/fi/hut/soberit/agilefant/readonly/ReadonlyFilter.java
@@ -15,8 +15,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.filter.GenericFilterBean;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+
+import fi.hut.soberit.agilefant.business.IterationBusiness;
 import fi.hut.soberit.agilefant.db.hibernate.IterationDAOHibernate;
 import fi.hut.soberit.agilefant.model.Iteration;
+
 
 public class ReadonlyFilter extends GenericFilterBean {
 
@@ -29,12 +34,33 @@ public class ReadonlyFilter extends GenericFilterBean {
         HttpServletResponse resp = (HttpServletResponse) response;
         HttpServletRequest reqt = (HttpServletRequest) request;
         
+        // Trying to set something up get things from the db. 
+        IterationDAOHibernate iterationDao = new IterationDAOHibernate();;
+        SessionFactory sessionFactory;
+        
+        try {
+            sessionFactory = (SessionFactory) new InitialContext().lookup("hibernateSessionFactory");
+            iterationDao.setSessionFactory(sessionFactory);
+        } catch (NamingException e) {
+            // TODO Auto-generated catch block
+            // Need to add error message here. 
+            e.printStackTrace();
+            return;
+        }
+        
+        // Open a session? This might not be a good idea. 
+        Session session = sessionFactory.openSession();
+        
         // Fetch url token from request.
         String urlToken = reqt.getPathInfo().substring(1);
         
         // Test url token against db(?) of known read-only urls. 
-        // Integer iterationId = fetchIterationIdFromURLToken(urlToken);
-        Integer iterationId = 9; // TODO @DF Dummy Need to figure out authentication.
+        Integer iterationId = 9; // TODO @DF Dummy... need to fetch this from our db table once things are actually working. 
+        
+        // Try to get data from db... this is where I'm stuck at the moment. 
+        Iteration i = iterationDao.retrieveDeep(iterationId);
+        
+        System.out.print(i.toString());
         
         // If url checks out, create iterationTO and supply it somewhere? 
         if (iterationId != null) {

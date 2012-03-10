e844d6e3dc36c12db4f01142a5ed84164e3d0e9c
jbaldwin <jebaldwin@gmail.com>
2012-03-10 15:26:58 -0800

    WorkItem: 4383 interceptor changes for admin-only actions

--- src/fi/hut/soberit/agilefant/web/AuthorizationInterceptor.java
+++ src/fi/hut/soberit/agilefant/web/AuthorizationInterceptor.java
@@ -2,28 +2,21 @@ package fi.hut.soberit.agilefant.web;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.Interceptor;
 
 import fi.hut.soberit.agilefant.business.BacklogBusiness;
-import fi.hut.soberit.agilefant.business.IterationBusiness;
-import fi.hut.soberit.agilefant.business.ProductBusiness;
-import fi.hut.soberit.agilefant.business.ProjectBusiness;
 import fi.hut.soberit.agilefant.business.UserBusiness;
-import fi.hut.soberit.agilefant.model.Backlog;
 import fi.hut.soberit.agilefant.model.Product;
-import fi.hut.soberit.agilefant.model.Project;
 import fi.hut.soberit.agilefant.model.Team;
 import fi.hut.soberit.agilefant.model.User;
 import fi.hut.soberit.agilefant.security.SecurityUtil;
-
-
 
 @Component("authorizationInterceptor")
 public class AuthorizationInterceptor implements Interceptor {
@@ -33,36 +26,44 @@ public class AuthorizationInterceptor implements Interceptor {
 
     @Autowired
     private BacklogBusiness backlogBusiness;
-    
    
     @Override
-    public void destroy() {
-        // TODO Auto-generated method stub
-
-    }
+    public void destroy() {}
 
     @Override
-    public void init() {
-        // TODO Auto-generated method stub
-
-    }
+    public void init() {}
 
     @Override
     public String intercept(ActionInvocation invocation) throws Exception {
         Object action = invocation.getAction();
         boolean accessDenied = false;
         
+        //matrix authorizations
         if (action instanceof BacklogAction){
             accessDenied = checkAccess(((BacklogAction) action).getBacklogId()); 
-
         } else if (action instanceof ProductAction) {
-            accessDenied = checkAccess(((ProductAction) action).getProductId());
-        
+            accessDenied = checkAccess(((ProductAction) action).getProductId());  
         } else if (action instanceof ProjectAction) {
-            accessDenied = checkAccess(((ProjectAction) action).getProjectId());
-        
+            accessDenied = checkAccess(((ProjectAction) action).getProjectId());      
         } else if (action instanceof IterationAction) {
             accessDenied = checkAccess(((IterationAction) action).getIterationId());
+        } else {
+            //admin authorizations
+            User currentUser = SecurityUtil.getLoggedUser();
+            boolean isAdmin = currentUser.isAdmin();
+            
+            if(isAdmin){
+                //admin has access to any actions
+                return invocation.invoke();
+            } else {
+                if(action instanceof AccessAction || 
+                   action instanceof DatabaseExportAction ||
+                   action instanceof SettingAction){
+                         return "notadmin";
+                } else {
+                    return invocation.invoke();
+                }
+            }
         }
         
         if (accessDenied) return "noauth";

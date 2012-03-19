cdec10bb875b6e4258dbf18b92e02f07f3b58f41
Braden Simpson <braden@uvic.ca>
2012-03-19 01:44:48 -0700

    atables tests

--- test/fi/hut/soberit/agilefant/db/export/AtablesmodifierTest.java
+++ test/fi/hut/soberit/agilefant/db/export/AtablesmodifierTest.java
@@ -1,6 +1,8 @@
 package fi.hut.soberit.agilefant.db.export;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,31 +11,47 @@ import java.sql.Statement;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException;
 
-public class AtablesmodifierTest {
+import fi.hut.soberit.agilefant.test.AbstractHibernateTests;
+
+@ContextConfiguration
+@Transactional
+public class AtablesmodifierTest extends AbstractHibernateTests{
 
     @Autowired
     private Atablesmodifier atablesmodifier;
     private Connection connection = null; 
     private Statement statement = null;
+    private String usr = "sa";
+    private String pwd = "sa";
+    private String url = "jdbc:h2:mem:aeftest";
+    private String driver = "org.h2.Driver";
     @Test
     public void testInitializeTables() {
+        atablesmodifier.dbinfo.setDbName("aeftest");
+        atablesmodifier.dbinfo.setUrl(url);
+        atablesmodifier.dbinfo.setHostname("h2");
+        atablesmodifier.dbinfo.setUsername(usr);
+        atablesmodifier.dbinfo.setPassword(pwd);
         
-        try {
-            atablesmodifier = new Atablesmodifier();            
-        }
-        catch(Exception ex) {
-            fail("Exception thrown");
-        }
+        executeClassSql();
+//        try {
+//            atablesmodifier = new Atablesmodifier();            
+//        }
+//        catch(Exception ex) {
+//            fail("Exception thrown");
+//        }
         assertFalse(atablesmodifier.getOriginalTables().isEmpty());       
     }
 
     @SuppressWarnings("unused")
     @Test
     public void testDuplicaTables() {
-        DbPropertiesReader properties = new DbPropertiesReader();
+        executeClassSql();
         try {
             atablesmodifier = new Atablesmodifier();
             atablesmodifier.dublicaTables();
@@ -41,13 +59,13 @@ public class AtablesmodifierTest {
         catch(Exception ex) {
             fail("Exception thrown");
         }
-        String sqlConnection = "jdbc:mysql://"+properties.getDbHost()+":3306"+ "/" +properties.getDbName();     //may need port in the future
+        String sqlConnection = url;
         int tablesize= atablesmodifier.getOriginalTables().size();
 
         try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Class.forName(driver).newInstance();
             connection = DriverManager.getConnection
-                    (sqlConnection, properties.getDbUsername(), properties.getDbPassword());
+                    (sqlConnection, usr, pwd);
             statement = connection.createStatement();
 
             ResultSet results = statement.executeQuery("SELECT * FROM anonym_settings");
@@ -65,7 +83,7 @@ public class AtablesmodifierTest {
 
     @Test
     public void testDeletetables() {
-        DbPropertiesReader properties = new DbPropertiesReader();
+        executeClassSql();
         try {
             atablesmodifier = new Atablesmodifier(); 
             atablesmodifier.dublicaTables();
@@ -74,13 +92,13 @@ public class AtablesmodifierTest {
         catch(Exception ex) {
             fail("Exception thrown");
         }
-        String sqlConnection = "jdbc:mysql://"+properties.getDbHost()+":3306"+ "/" +properties.getDbName();     //may need port in the future
+        String sqlConnection = url; 
         int tablesize= atablesmodifier.getOriginalTables().size();
 
         try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            Class.forName(driver).newInstance();
             connection = DriverManager.getConnection
-                    (sqlConnection, properties.getDbUsername(), properties.getDbPassword());
+                    (sqlConnection, usr, pwd);
             statement = connection.createStatement();
 
             ResultSet results = statement.executeQuery("SELECT * FROM anonym_settings");

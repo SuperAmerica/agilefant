b3e519e7d6dd1e29bc9175edaeeacedf0a1c7dfe
Jordan Ell <jordan.ell7@gmail.com>
2012-03-23 12:21:07 -0700

    WorkItem: 4722 Got the upgrader SQL queries working.

--- doc/sql/upgrader/250-300.sql
+++ doc/sql/upgrader/250-300.sql
@@ -6,6 +6,18 @@ alter table team_product add index FK65CE090DA7FE2362 (Product_id), add constrai
 ALTER TABLE backlogs ADD COLUMN readonlyToken varchar(255) unique;
 INSERT INTO users (admin, loginName, enabled, recentItemsNumberOfWeeks) VALUES (0, "readonly", 1, 0);
 
+-- Give all teams access to all products
+INSERT INTO team_product (Team_id, Product_id)
+SELECT A.id, B.id
+FROM (SELECT id FROM teams) A,
+     (SELECT id FROM backlogs WHERE backlogtype='Product') B;
+
+-- Give all teams access to all standalone iterations
+INSERT INTO team_iteration (Team_id, Iteration_id)
+SELECT A.id, B.id
+FROM (SELECT id FROM teams) A,
+     (SELECT id FROM backlogs WHERE backlogtype='Iteration' AND parent_id IS NULL) B;
+
 -- Access rights for stand alone iterations
 create table team_iteration (Team_id integer not null, Iteration_id integer not null) ENGINE=InnoDB;
 alter table team_iteration add index FKF2269B7B4157D2A2 (Iteration_id), add constraint FKF2269B7B4157D2A2 foreign key (Iteration_id) references backlogs (id);
